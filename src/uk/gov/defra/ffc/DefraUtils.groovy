f09f5feb1002995a8cf053c353d33e7aa6c46b32
markysoft <mark.harrop@defra.gov.uk>
2019-12-18 15:43:38 +0000

    move test out deletion to own function

--- src/uk/gov/defra/ffc/DefraUtils.groovy
+++ src/uk/gov/defra/ffc/DefraUtils.groovy
@@ -85,9 +85,12 @@ def runTests(name, suffix) {
   } finally {
     sh "docker-compose -p $name-$suffix-$containerTag -f docker-compose.yaml -f docker-compose.test.yaml down -v"
     junit 'test-output/junit.xml'
-    // clean up files created by node/ubuntu user that cannot be deleted by jenkins. Note: uses global environment variable
-    sh "docker run --rm -u node --mount type=bind,source='$WORKSPACE/test-output',target=/usr/src/app/test-output $name rm -rf test-output/*"
   }
+}
+
+def deleteTestOutput(name) {
+    // clean up files created by node/ubuntu user that cannot be deleted by jenkins. Note: uses global environment variable
+    sh "docker run --rm -u node --mount type=bind,source='$WORKSPACE/test-output',target=/usr/src/app/test-output $name rm -rf test-output/*"  
 }
 
 def analyseCode(sonarQubeEnv, sonarScanner, params) {
