6d3f6b9975775be1bda5d77138b7004610c10e37
markysoft <mark.harrop@defra.gov.uk>
2019-12-19 11:20:57 +0000

    add and document replaceInFile function

--- src/uk/gov/defra/ffc/DefraUtils.groovy
+++ src/uk/gov/defra/ffc/DefraUtils.groovy
@@ -7,6 +7,10 @@ def repoUrl = ''
 def commitSha = ''
 def workspace
 
+def replaceInFile(from, to, file) {
+  sh "sed -i -e 's/$from/$to/g' $file"  
+}
+
 def getMergedPrNo() {
     def mergedPrNo = sh(returnStdout: true, script: "git log --pretty=oneline --abbrev-commit -1 | sed -n 's/.*(#\\([0-9]\\+\\)).*/\\1/p'").trim()
     return mergedPrNo ? "pr$mergedPrNo" : ''
