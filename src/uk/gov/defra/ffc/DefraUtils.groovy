a00684f03a1d39b28972b53e601a4f7448b19f20
Matthew Quinton <matthew.quinton@rpa.gsi.gov.uk>
2020-02-05 14:07:54 +0000

    put if back to poper state

--- src/uk/gov/defra/ffc/DefraUtils.groovy
+++ src/uk/gov/defra/ffc/DefraUtils.groovy
@@ -186,7 +186,7 @@ def notifySlackBuildFailure(exception, channel) {
           ${exception}
           (<${BUILD_URL}|Open>)"""
 
-  if(!JOB_NAME.contains("/master/")) {
+  if(JOB_NAME.contains("/master/")) {
     msg = '@here '.concat(msg);
     channel = "#masterbuildfailures"
   }
