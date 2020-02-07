7e0bc5372eb77412580feb480e49756039b5e347
Nick Blows <nicholasblows@rpa.gov.uk>
2020-02-07 12:30:12 +0000

    Added new param and logic

--- src/uk/gov/defra/ffc/DefraUtils.groovy
+++ src/uk/gov/defra/ffc/DefraUtils.groovy
@@ -39,13 +39,20 @@ def verifyCommitBuildable() {
     }
 }
 
-def getVariables(repoName) {
+def getVariables(repoName, version) {
     branch = BRANCH_NAME
     // use the git API to get the open PR for a branch
     // Note: This will cause issues if one branch has two open PRs
     pr = sh(returnStdout: true, script: "curl https://api.github.com/repos/DEFRA/$repoName/pulls?state=open | jq '.[] | select(.head.ref == \"$branch\") | .number'").trim()
     verifyCommitBuildable()
-    def rawTag = pr == '' ? branch : "pr$pr"
+      
+    def rawTag
+    if (branch == "master") {
+      rawTag = version
+    } else {
+      rawTag = pr == '' ? branch : "pr$pr"
+    }
+
     containerTag = rawTag.replaceAll(/[^a-zA-Z0-9]/, '-').toLowerCase()
     mergedPrNo = getMergedPrNo()
     repoUrl = getRepoUrl()
