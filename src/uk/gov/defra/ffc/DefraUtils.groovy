6f63039a0977163f9e88a71c3f6f1f2ff710aea4
Nick Blows <nicholasblows@rpa.gov.uk>
2020-02-12 16:21:29 +0000

    Amended release check

--- src/uk/gov/defra/ffc/DefraUtils.groovy
+++ src/uk/gov/defra/ffc/DefraUtils.groovy
@@ -186,8 +186,8 @@ def triggerDeploy(jenkinsUrl, jobName, token, params) {
 def releaseExists(containerTag, repoName, token){
     
     //temp
-    containerTag = "1.0.3"
-    //boolean doesReleaseExist = false
+    containerTag = "1.0.5"
+    doesReleaseExist = false
      //latestReleaseNum = sh(returnStatus: true, script: "curl --silent -H 'Authorization: token $token' https://api.github.com/repos/DEFRA/$repoName/releases/latest |jq '.tag_name'")  
     doesReleaseExist = sh(returnStdout: true, script: "curl --silent -H 'Authorization: token $token' https://api.github.com/repos/DEFRA/$repoName/releases | jq '.[].tag_name | index(\"$containerTag\") | select (. != null) | tostring | test(\"0\")'").trim()
     echo "containerTag value is $containerTag"
