66d9845eba4560f5bc327a33a17826ae9993ad7e
Nick Blows <nicholasblows@rpa.gov.uk>
2020-02-13 21:46:32 +0000

    amended release trigger

--- src/uk/gov/defra/ffc/DefraUtils.groovy
+++ src/uk/gov/defra/ffc/DefraUtils.groovy
@@ -185,10 +185,11 @@ def triggerDeploy(jenkinsUrl, jobName, token, params) {
 
 def releaseExists(containerTag, repoName, token){
     def doesReleaseExist = false
-    def result = sh(returnStdout: true, script: "curl -s -H 'Authorization: token $token' https://api.github.com/repos/DEFRA/$repoName/releases/tags/$containerTag | jq '.tag_name'") 
-    echo "The container tag is \"$containerTag\""
+    String containerTagString = containerTag
+    String result = sh(returnStdout: true, script: "curl -s -H 'Authorization: token $token' https://api.github.com/repos/DEFRA/$repoName/releases/tags/$containerTag | jq '.tag_name'") 
+    echo "The container tag is $containerTag"
     echo "The result is $result"
-    if (result == "$containerTag"){
+    if (result == containerTagString){
       echo "Release exists!"
       doesReleaseExist = true
     } else {
