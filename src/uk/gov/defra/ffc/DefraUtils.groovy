65ac1adec2281f42e70038399e19edd00bbbf773
Nick Blows <nicholasblows@rpa.gov.uk>
2020-02-14 10:13:32 +0000

    amended release trigger

--- src/uk/gov/defra/ffc/DefraUtils.groovy
+++ src/uk/gov/defra/ffc/DefraUtils.groovy
@@ -208,7 +208,7 @@ def triggerRelease(containerTag, repoName, releaseDescription, token){
 
     echo "Triggering release $containerTag for $repoName"
     boolean result = false
-    result = sh(returnStdout: true, script: "curl -X POST -H 'Authorization: token $token' -d '{ \"tag_name\" : \"$containerTag\", \"name\" : \"Release $containerTag\", \"body\" : \" Release $releaseDescription\" }' https://api.github.com/repos/DEFRA/$repoName/releases")
+    result = sh(returnStdout: true, script: "curl -s -X POST -H 'Authorization: token $token' -d '{ \"tag_name\" : \"$containerTag\", \"name\" : \"Release $containerTag\", \"body\" : \" Release $releaseDescription\" }' https://api.github.com/repos/DEFRA/$repoName/releases")
     echo "The release result is $result"
 
     if (releaseExists(containerTag, repoName, token)){
