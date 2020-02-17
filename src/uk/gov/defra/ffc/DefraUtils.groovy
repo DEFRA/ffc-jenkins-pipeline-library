31f677c9cd690d1eceb3a520d4446b03c33e2a1d
David Bingham <david.bingham@defra.gov.uk>
2020-02-17 14:18:11 +0000

    Revert "Accept custom command to run tests"
    This reverts commit 5ddbcfdf91b6cfc04a87f1be49b3abc098a45150.

--- src/uk/gov/defra/ffc/DefraUtils.groovy
+++ src/uk/gov/defra/ffc/DefraUtils.groovy
@@ -126,11 +126,11 @@ def buildTestImage(projectName, serviceName, buildNumber) {
   sh "docker-compose -p $projectName-$containerTag-$buildNumber -f docker-compose.yaml -f docker-compose.test.yaml build --no-cache $serviceName"
 }
 
-def runTests(projectName, serviceName, command, buildNumber) {
+def runTests(projectName, serviceName, buildNumber) {
   try {
     sh 'mkdir -p test-output'
     sh 'chmod 777 test-output'
-    sh "docker-compose -p $projectName-$containerTag-$buildNumber -f docker-compose.yaml -f docker-compose.test.yaml run $serviceName $command"
+    sh "docker-compose -p $projectName-$containerTag-$buildNumber -f docker-compose.yaml -f docker-compose.test.yaml run $serviceName"
   } finally {
     sh "docker-compose -p $projectName-$containerTag-$buildNumber -f docker-compose.yaml -f docker-compose.test.yaml down -v"
   }
