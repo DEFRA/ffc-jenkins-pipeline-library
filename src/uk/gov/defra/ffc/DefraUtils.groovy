1ba30f0565efbccd65359d35bebf4f7d2749baf0
Phil Benson <phil.benson@defra.gov.uk>
2020-02-27 16:09:42 +0000

    When calling helm upgrade, pass the namespace as a variable rather than using the --namespace switch

--- src/uk/gov/defra/ffc/DefraUtils.groovy
+++ src/uk/gov/defra/ffc/DefraUtils.groovy
@@ -304,7 +304,7 @@ def deployChart(credentialsId, registry, chartName, tag, extraCommands) {
   withKubeConfig([credentialsId: credentialsId]) {
     def deploymentName = "$chartName-$tag"
     sh "kubectl get namespaces $deploymentName || kubectl create namespace $deploymentName"
-    sh "helm upgrade $deploymentName --install --namespace $deploymentName --atomic ./helm/$chartName --set image=$registry/$chartName:$tag $extraCommands"
+    sh "helm upgrade $deploymentName --install --atomic ./helm/$chartName --set image=$registry/$chartName:$tag,namespace=$deploymentName $extraCommands"
   }
 }
 
