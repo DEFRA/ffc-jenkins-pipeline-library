5f623107784b5d078e3ed9650d71561eae6b2684
David Bingham <git@davidjbingham.co.uk>
2020-03-11 12:01:31 +0000

    Add deployRemoteChart function
    Based on deployment pipelines for ffc-demo services.

--- src/uk/gov/defra/ffc/DefraUtils.groovy
+++ src/uk/gov/defra/ffc/DefraUtils.groovy
@@ -370,6 +370,15 @@ def deployChart(credentialsId, registry, chartName, tag, extraCommands) {
   }
 }
 
+def deployRemoteChart(namespace, chartName, chartVersion, extraCommands) {
+  withKubeConfig([credentialsId: KUBE_CREDENTIALS_ID]) {
+    sh "helm repo add ffc-demo $HELM_CHART_REPO"
+    sh "helm repo update"
+    sh "helm fetch --untar ffc-demo/$chartName --version $chartVersion"
+    sh "helm upgrade --install --recreate-pods --wait --atomic $chartName --set namespace=$namespace ./$chartName $extraCommands"
+  }
+}
+
 def undeployChart(credentialsId, chartName, tag) {
   def deploymentName = "$chartName-$tag"
   echo "removing deployment $deploymentName"
