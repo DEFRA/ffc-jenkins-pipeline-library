dfbfe14de77d47707e49deae36e9f7845b9316ab
David Bingham <djbingham@users.noreply.github.com>
2020-03-13 10:04:44 +0000

    Add deployRemoteChart function (#33)

--- src/uk/gov/defra/ffc/DefraUtils.groovy
+++ src/uk/gov/defra/ffc/DefraUtils.groovy
@@ -404,6 +404,15 @@ def publishChart(registry, chartName, tag) {
   }
 }
 
+def deployRemoteChart(namespace, chartName, chartVersion, extraCommands) {
+  withKubeConfig([credentialsId: KUBE_CREDENTIALS_ID]) {
+    sh "helm repo add ffc-demo $HELM_CHART_REPO"
+    sh "helm repo update"
+    sh "helm fetch --untar ffc-demo/$chartName --version $chartVersion"
+    sh "helm upgrade --install --recreate-pods --wait --atomic $chartName --set namespace=$namespace ./$chartName $extraCommands"
+  }
+}
+
 def triggerDeploy(jenkinsUrl, jobName, token, params) {
   def url = "$jenkinsUrl/job/$jobName/buildWithParameters?token=$token"
   params.each { param ->
