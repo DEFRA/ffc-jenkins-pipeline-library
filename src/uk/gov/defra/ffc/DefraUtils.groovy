7023c3bd9c316eea7496589c48d6271675efa2fb
Phil Benson <phil.benson@defra.gov.uk>
2020-03-13 17:41:29 +0000

    added a ton of debug logging

--- src/uk/gov/defra/ffc/DefraUtils.groovy
+++ src/uk/gov/defra/ffc/DefraUtils.groovy
@@ -74,18 +74,24 @@ def provisionPrSqsQueue(serviceCode, serviceName, serviceType, prCode, queuePurp
     final int SQS_NAME_CHAR_LIMIT = 76
     assert serviceName.size() + prCode.toString().size() + queuePurpose.size() < SQS_NAME_CHAR_LIMIT :
       "service name, pr code and queue purpose parameters should have fewer than 76 characters when combined";
+    echo "changing to terragrunt dir"
     dir('terragrunt') {
+      echo "withCredentials"
        withCredentials([
         string(credentialsId: 'ffc-jenkins-pipeline-terragrunt-repo', variable: 'tg_repo_url'),
         string($class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'devffcprovision-user')
       ]) {
         sh "pwd"
         // git clone repo...
+        echo "cloning repo"
         git credentialsId: 'helm-chart-creds', url: tg_repo_url
         echo "cloned repo"
 
+        echo "changing to dir london/eu-west-2/ffc"
         dir('london/eu-west-2/ffc') {
+          sh "pwd"
           def dirName = "${serviceName}-pr${prCode}-${queuePurpose}"
+          echo "checking for existing dir"
           if (!fileExists("${dirName}/terraform.tfvars")) {
             echo "${dirName} directory doesn't exist, creating..."
             echo "create new dir from model dir, then add to git"
@@ -98,6 +104,8 @@ def provisionPrSqsQueue(serviceCode, serviceName, serviceType, prCode, queuePurp
               echo "provision infrastructure"
               sh "terragrunt apply -var-file='vars.tfvars' -auto-approve --terragrunt-non-interactive"
             }
+          } else {
+            echo "directory exists, presuming TG / TF has been run"
           }
         }
         // Recursively delete the current dir (which should be terragrunt in the current job workspace)
