c479db57f2f4316ff0e5d1834957e57913112cc9
Phil Benson <phil.benson@defra.gov.uk>
2020-03-18 16:47:26 +0000

    Switch to helm3 in deployRemoteChart

--- src/uk/gov/defra/ffc/DefraUtils.groovy
+++ src/uk/gov/defra/ffc/DefraUtils.groovy
@@ -390,10 +390,10 @@ def publishChart(registry, chartName, tag) {
 
 def deployRemoteChart(namespace, chartName, chartVersion, extraCommands) {
   withKubeConfig([credentialsId: KUBE_CREDENTIALS_ID]) {
-    sh "helm repo add ffc-demo $HELM_CHART_REPO"
-    sh "helm repo update"
-    sh "helm fetch --untar ffc-demo/$chartName --version $chartVersion"
-    sh "helm upgrade --install --recreate-pods --wait --atomic $chartName --set namespace=$namespace ./$chartName $extraCommands"
+    sh "helm3 repo add ffc-demo $HELM_CHART_REPO"
+    sh "helm3 repo update"
+    sh "helm3 fetch --untar ffc-demo/$chartName --version $chartVersion"
+    sh "helm3 upgrade --install --recreate-pods --wait --atomic $chartName --set namespace=$namespace ./$chartName $extraCommands"
   }
 }
 
