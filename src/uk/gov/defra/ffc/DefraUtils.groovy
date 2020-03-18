93fe287096e6dfb2ab7fc9d01b7841003f783d83
Phil Benson <phil.benson@defra.gov.uk>
2020-03-18 15:13:20 +0000

    Switched from helm delete to helm uninstall, also removed --purge switch as it's no longer needed

--- src/uk/gov/defra/ffc/DefraUtils.groovy
+++ src/uk/gov/defra/ffc/DefraUtils.groovy
@@ -359,7 +359,7 @@ def undeployChart(credentialsId, chartName, tag) {
   def deploymentName = "$chartName-$tag"
   echo "removing deployment $deploymentName"
   withKubeConfig([credentialsId: credentialsId]) {
-    sh "helm delete --purge $deploymentName || echo error removing deployment $deploymentName"
+    sh "helm delete || echo error removing deployment $deploymentName"
     sh "kubectl delete namespaces $deploymentName || echo error removing namespace $deploymentName"
   }
 }
