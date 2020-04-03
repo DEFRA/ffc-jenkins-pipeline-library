78fd3f8c25e2424eaf4763380d3d0f27b8b3c392
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-04-03 10:15:43 +0100

    Changes required for pipeline to function
    Involved brining the docker steps into the github file as this is where
    a required variable is set. This will be refactored at a later point.

--- vars/github.groovy
+++ vars/github.groovy
@@ -80,3 +80,30 @@ def setGithubStatusPending(message = 'Build started') {
 def setGithubStatusFailure(message = '') {
   updateGithubCommitStatus(message, 'FAILURE')
 }
+
+// public
+def buildTestImage(credentialsId, registry, projectName, buildNumber) {
+  docker.withRegistry("https://$registry", credentialsId) {
+    sh "docker-compose -p $projectName-$containerTag-$buildNumber -f docker-compose.yaml -f docker-compose.test.yaml build --no-cache"
+  }
+}
+
+// public
+def runTests(projectName, serviceName, buildNumber) {
+  try {
+    sh 'mkdir -p test-output'
+    sh 'chmod 777 test-output'
+    sh "docker-compose -p $projectName-$containerTag-$buildNumber -f docker-compose.yaml -f docker-compose.test.yaml run $serviceName"
+  } finally {
+    sh "docker-compose -p $projectName-$containerTag-$buildNumber -f docker-compose.yaml -f docker-compose.test.yaml down -v"
+  }
+}
+
+// public
+def buildAndPushContainerImage(credentialsId, registry, imageName, tag) {
+  docker.withRegistry("https://$registry", credentialsId) {
+    sh "docker-compose -f docker-compose.yaml build --no-cache"
+    sh "docker tag $imageName $registry/$imageName:$tag"
+    sh "docker push $registry/$imageName:$tag"
+  }
+}
--- vars/test.groovy
+++ vars/test.groovy
@@ -4,17 +4,6 @@ def lintHelm(chartName) {
 }
 
 // public
-def runTests(projectName, serviceName, buildNumber) {
-  try {
-    sh 'mkdir -p test-output'
-    sh 'chmod 777 test-output'
-    sh "docker-compose -p $projectName-$containerTag-$buildNumber -f docker-compose.yaml -f docker-compose.test.yaml run $serviceName"
-  } finally {
-    sh "docker-compose -p $projectName-$containerTag-$buildNumber -f docker-compose.yaml -f docker-compose.test.yaml down -v"
-  }
-}
-
-// public
 def createTestReportJUnit(){
   junit 'test-output/junit.xml'
 }
