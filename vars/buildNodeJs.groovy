874e414a7dd72ac52ef1360c6850c6dd58296f79
Phil Benson <phil.benson@defra.gov.uk>
2020-04-07 16:40:38 +0100

    Test undeployChart

--- vars/buildNodeJs.groovy
+++ vars/buildNodeJs.groovy
@@ -20,6 +20,7 @@ def call(Map config=[:], Closure body={}) {
       }
       stage('Set PR, and containerTag variables') {
         (repoName, pr, containerTag, mergedPrNo) = build.getVariables(version.getPackageJsonVersion())
+        mergedPrNo = '118'
       }
       stage('Helm lint') {
         test.lintHelm()
