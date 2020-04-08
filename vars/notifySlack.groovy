3fcb1f2b2625b4330ab687e07560cac79078d15f
Phil Benson <phil.benson@defra.gov.uk>
2020-04-08 09:36:37 +0100

    add logging to buildFailure

--- vars/notifySlack.groovy
+++ vars/notifySlack.groovy
@@ -1,16 +1,20 @@
 // public
 def buildFailure(exception, channel) {
+  echo "build message"
   def msg = """BUILD FAILED
           ${JOB_NAME}/${BUILD_NUMBER}
           ${exception}
           (<${BUILD_URL}|Open>)"""
+  echo "built message: ${msg}"
 
   if(branch == "master") {
     msg = "@here ${msg}"
     channel = "#masterbuildfailures"
   }
 
+  echo "sending to slack"
   slackSend channel: channel,
             color: "#ff0000",
             message: msg.replace("  ", "")
+  echo "sent to slace"
 }
