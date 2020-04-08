1bb6811fc5054db76748b7711178f08faca149f3
Phil Benson <phil.benson@defra.gov.uk>
2020-04-08 12:28:07 +0100

    defining branch

--- vars/buildNodeJs.groovy
+++ vars/buildNodeJs.groovy
@@ -21,7 +21,7 @@ def call(Map config=[:], Closure body={}) {
       stage('Set PR, and containerTag variables') {
         (repoName, pr, containerTag, mergedPrNo) = build.getVariables(version.getPackageJsonVersion())
       }
-      echo "branch name: ${branch}"
+      echo "branch name: ${BRANCH_NAME}"
       notifySlack.buildFailure("I can't do that Dave", "#generalbuildfailures")
       /*stage('Helm lint') {
         test.lintHelm()
