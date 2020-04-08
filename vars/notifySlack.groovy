2e184555ace0a69c8f391dfde3d338095e59e64a
Phil Benson <phil.benson@defra.gov.uk>
2020-04-08 11:58:21 +0100

    More logging...

--- vars/notifySlack.groovy
+++ vars/notifySlack.groovy
@@ -7,17 +7,19 @@ def buildFailure(exception, channel) {
           (<${BUILD_URL}|Open>)"""
   echo "built message: ${msg}£££"
   try {
+    echo "=== 1 ==="
     echo "branch name ${branch}"
+    echo "=== 2 ==="
   } catch (e) {
     echo "Error: ${e.message}"
   }
 
   if(branch == "master") {
-    echo "=== 1 ==="
-    msg = "@here ${msg}"
-    echo "=== 2 ==="
-    channel = "#masterbuildfailures"
     echo "=== 3 ==="
+    msg = "@here ${msg}"
+    echo "=== 4 ==="
+    channel = "#masterbuildfailures"
+    echo "=== 5 ==="
   }
 
   echo "sending to slack"
