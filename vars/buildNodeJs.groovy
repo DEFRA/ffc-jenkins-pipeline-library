4197ce60cb2d3028b4861980837f29cfc51c116c
Phil Benson <phil.benson@defra.gov.uk>
2020-04-09 12:42:46 +0100

    Test out deployment after we've made some changes

--- vars/buildNodeJs.groovy
+++ vars/buildNodeJs.groovy
@@ -18,6 +18,7 @@ def call(Map config=[:], Closure body={}) {
       }
       stage('Set PR, and containerTag variables') {
         (repoName, pr, containerTag, mergedPrNo) = build.getVariables(version.getPackageJsonVersion())
+        pr = ''
       }
       stage('Helm lint') {
         test.lintHelm(repoName)
