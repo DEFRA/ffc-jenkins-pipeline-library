378715b0f54369054230db6639c0c678326b6834
John Watson <john.watson2@rpa.gov.uk>
2020-04-10 14:38:11 +0100

    Fix deployment

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,9 +1,15 @@
 // public
 def trigger(jenkinsUrl, repoName, token, params) {
-  def url = "$jenkinsUrl/job/$repoName-deploy/buildWithParameters?token=$token"
+  def folder = getFolder(repoName)
+  def url = "$jenkinsUrl/job/$folder/job/$repoName-deploy/buildWithParameters?token=$token"
   params.each { param ->
     url = url + "\\&$param.key=$param.value"
   }
   echo "Triggering deployment for $url"
   sh(script: "curl -k $url")
+}
+
+def folder(repoName) {
+  def folderArray = repoName.split('-')
+  return "${folderArray[0]}-${folderArray[1]}"
 }
