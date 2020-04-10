bd9580234876eabfa1d1358718bf277065caac3f
John Watson <john.watson2@rpa.gov.uk>
2020-04-10 14:36:49 +0100

    Folder removed

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,15 +1,10 @@
 // public
 def trigger(jenkinsUrl, repoName, token, params) {
   def folder = getFolder(repoName)
-  def url = "$jenkinsUrl/job/$folder/job/$repoName-deploy/buildWithParameters?token=$token"
+  def url = "$jenkinsUrl/job/$repoName-deploy/buildWithParameters?token=$token"
   params.each { param ->
     url = url + "\\&$param.key=$param.value"
   }
   echo "Triggering deployment for $url"
   sh(script: "curl -k $url")
-}
-
-def folder(repoName) {
-  def folderArray = repoName.split('-')
-  return "${folderArray[0]}-${folderArray[1]}"
 }
