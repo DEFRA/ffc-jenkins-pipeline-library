b4b2675a0e166bf3c443290438f97381e0288f6c
John Watson <johnwatson484@hotmail.com>
2020-04-11 11:56:17 +0100

    deployToClusterPipeline added

---
+++ vars/deployToCluster.groovy
@@ -0,0 +1,13 @@
+def call(Map config=[:], Closure body={}) {
+  node {
+    checkout scm
+    try {
+      stage('Deploy Helm chart') {
+        helm.deployRemoteChart(config.environment, config.namespace, config.chartName, config.chartVersion)
+      }      
+      body()
+    } catch(e) {
+      notifySlack.buildFailure(e.message, "#generalbuildfailures")
+      throw e
+  }
+}
