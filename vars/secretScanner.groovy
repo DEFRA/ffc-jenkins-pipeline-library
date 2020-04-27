04e689fcae109b419d45bb347d2f7f2850cfbd68
Paul Andrews <paul.andrews@defra.gov.uk>
2020-04-28 00:39:23 +0100

    tidy up

--- vars/secretScanner.groovy
+++ vars/secretScanner.groovy
@@ -47,35 +47,27 @@ def scanWithinWindow(githubOrg, repositoryPrefix, scanWindowHrs) {
 
     sh "docker pull dxa4481/trufflehog"
 
-    try {
-      matchingRepos.each {
-        // The truffleHog docker run cause exit code 1 which fails the build so need the || true to ignore it
-        def truffleHogCmd = "docker run dxa4481/trufflehog --json --regex https://github.com/${it}.git || true"
-        def truffleHogRes = sh(returnStdout: true, script: truffleHogCmd).trim()
-        def reportRes = []
-        def jsonSlurper = new JsonSlurper()
+    matchingRepos.each {
+      // The truffleHog docker run cause exit code 1 which fails the build so need the || true to ignore it
+      def truffleHogCmd = "docker run dxa4481/trufflehog --json --regex https://github.com/${it}.git || true"
+      def truffleHogRes = sh(returnStdout: true, script: truffleHogCmd).trim()
+      def reportRes = []
+      def jsonSlurper = new JsonSlurper()
 
-        echo "HERE 2"
+      truffleHogRes.split('\n').each {
+        def result = jsonSlurper.parseText(it)
+        def dateObj = new Date().parse("yyyy-MM-dd HH:mm:ss", result.date)
 
-        truffleHogRes.split('\n').each {
-          echo "HERE 3"
-          def result = jsonSlurper.parseText(it)
-          def dateObj = new Date().parse("yyyy-MM-dd HH:mm:ss", result.date)
-
-          if (dateObj > commitCheckDate) {
-            def message = "Reason: $result.reason\n" +
-                          "Date: $result.date\n" +
-                          "Hash: $result.commitHash\n" +
-                          "Filepath: $result.path\n" +
-                          "Branch: $result.branch\n" +
-                          "Commit: $result.commit\n"
-            print message
-          }
+        if (dateObj > commitCheckDate) {
+          def message = "Reason: $result.reason\n" +
+                        "Date: $result.date\n" +
+                        "Hash: $result.commitHash\n" +
+                        "Filepath: $result.path\n" +
+                        "Branch: $result.branch\n" +
+                        "Commit: $result.commit\n"
+          print message
         }
       }
-    } catch (e) {
-      echo "Kaboom"
-      echo "$e"
     }
   }
 }
