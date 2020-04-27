5c3168e04de3c4fabf9af7a43bad79ba0fd9566e
Paul Andrews <paul.andrews@defra.gov.uk>
2020-04-27 23:44:00 +0100

    calculate commit check date and pull docker image

--- vars/secretScanner.groovy
+++ vars/secretScanner.groovy
@@ -3,7 +3,7 @@ def scanWithinWindow(githubOrg, repositoryPrefix, scanWindowHrs) {
     def curlAuth = "curl --header 'Authorization: token $githubToken' --silent"
     def githubApiUrl = "https://api.github.com/orgs/$githubOrg/repos?per_page=100"
 
-    def curlHeaders = sh(returnStdout: true, script: "$curlAuth --head $githubApiUrl")
+    def curlHeaders = sh returnStdout: true, script: "$curlAuth --head $githubApiUrl"
     def numPages = "1"
 
     curlHeaders.split('\n').each {
@@ -19,7 +19,7 @@ def scanWithinWindow(githubOrg, repositoryPrefix, scanWindowHrs) {
     (numPages as Integer).times {
       def reposCmd = "$curlAuth $githubApiUrl\\&page=${it+1} | jq '.[] | .full_name'"
       // FIXME: look into reading this into groovy JSON object instead of using jq
-      def reposResult = sh(returnStdout: true, script: reposCmd).trim().replaceAll (/"/, '')
+      def reposResult = sh returnStdout: true, script: reposCmd).trim().replaceAll (/"/, ''
 
       reposResult.split('\n').each {
         if (it.contains(repositoryPrefix)) {
@@ -29,6 +29,17 @@ def scanWithinWindow(githubOrg, repositoryPrefix, scanWindowHrs) {
     }
 
     echo "Matching repos: $matchingRepos"
+
+    def commitCheckDate
+
+    use (groovy.time.TimeCategory) {
+      commitCheckDate = new Date() - scanWindowHrs.hours
+    }
+
+    echo "Commit check date: $commitCheckDate"
+
+    sh "docker pull dxa4481/trufflehog"
+
 
   }
 }
