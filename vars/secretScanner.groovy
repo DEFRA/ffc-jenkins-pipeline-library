f33e9fffa0f13b11d6058b7695fa19f245edc74c
Paul Andrews <paul.andrews@defra.gov.uk>
2020-04-27 23:14:15 +0100

    fix get headers

--- vars/secretScanner.groovy
+++ vars/secretScanner.groovy
@@ -4,17 +4,14 @@ def scanWithinWindow(githubOrg, repositoryPrefix, scanWindowHrs) {
     def githubApiUrl = "https://api.github.com/orgs/$githubOrg/repos?per_page=100"
 
     def curlHeaders = sh(returnStdout: true, script: "$curlAuth --head $githubApiUrl")
-    // def curlHeaders = getHeadersCurlCmd.execute().text.split('\n')
     def numPages = "1"
 
-    echo "$curlHeaders"
-
-    curlHeaders.each {
+    curlHeaders.split('\n').each {
       if (it.startsWith('Link:')) {
         numPages = it[(it.lastIndexOf('&page=')+6)..(it.lastIndexOf('>')-1)]
       }
     }
 
-    echo "$numPages"
+    echo "Number of pages of repos: $numPages"
   }
 }
