622a1b31a2b2aa858ec8978b0c6c5f6b75149682
Paul Andrews <paul.andrews@defra.gov.uk>
2020-04-28 00:24:15 +0100

    debugging

--- vars/secretScanner.groovy
+++ vars/secretScanner.groovy
@@ -49,7 +49,7 @@ def scanWithinWindow(githubOrg, repositoryPrefix, scanWindowHrs) {
 
     try {
       matchingRepos.each {
-        def truffleHogCmd = "docker run dxa4481/trufflehog --json --regex https://github.com/${it}.git"
+        def truffleHogCmd = "set +x && docker run dxa4481/trufflehog --json --regex https://github.com/${it}.git"
         def truffleHogRes = sh(returnStdout: true, script: truffleHogCmd).trim()
 
         echo "HERE 1"
