7efa047583212128502d15510d0ccbd4aebd25ad
Paul Andrews <paul.andrews@defra.gov.uk>
2020-04-28 00:05:22 +0100

    debugging

--- vars/secretScanner.groovy
+++ vars/secretScanner.groovy
@@ -11,7 +11,8 @@ def getCommitCheckDate(scanWindowHrs) {
 def scanWithinWindow(githubOrg, repositoryPrefix, scanWindowHrs) {
   withCredentials([string(credentialsId: 'github-auth-token', variable: 'githubToken')]) {
     def curlAuth = "curl --header 'Authorization: token $githubToken' --silent"
-    def githubApiUrl = "https://api.github.com/orgs/$githubOrg/repos?per_page=100"
+    // def githubApiUrl = "https://api.github.com/orgs/$githubOrg/repos?per_page=100"
+    def githubApiUrl = "https://api.github.com/users/$githubOrg/repos?per_page=100"
 
     def curlHeaders = sh returnStdout: true, script: "$curlAuth --head $githubApiUrl"
     def numPages = "1"
