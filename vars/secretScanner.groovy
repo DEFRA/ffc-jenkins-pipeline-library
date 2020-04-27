6bf370f8b845f067e53746a2b535217ddd7b7d09
Paul Andrews <paul.andrews@defra.gov.uk>
2020-04-27 23:30:28 +0100

    debugging

--- vars/secretScanner.groovy
+++ vars/secretScanner.groovy
@@ -17,7 +17,7 @@ def scanWithinWindow(githubOrg, repositoryPrefix, scanWindowHrs) {
     def matchingRepos = []
 
     (numPages as Integer).times {
-      def reposCmd = "$curlAuth $githubApiUrl | jq '.[] | .full_name' | grep $githubOrg\\/$repositoryPrefix"
+      def reposCmd = "$curlAuth $githubApiUrl | jq '.[] | .full_name'"
       echo "$reposCmd"
 
       // FIXME: look into reading this into groovy JSON object instead of using jq
