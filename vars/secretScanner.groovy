3e6bd8bc00062b53e9ce785b67816b84b64ea89c
Paul Andrews <paul.andrews@defra.gov.uk>
2020-04-28 20:33:32 +0100

    use github api for commits

--- vars/secretScanner.groovy
+++ vars/secretScanner.groovy
@@ -49,49 +49,58 @@ def scanWithinWindow(githubUser, repositoryPrefix, scanWindowHrs) {
 
     sh "docker pull dxa4481/trufflehog"
 
+    def jsonSlurper = new JsonSlurper()
+
     matchingRepos.each {
       echo "Scanning $it"
+
+      def githubApiCommitUrl = "https://api.github.com/repos/$it/commits?since=$commitCheckDate"
+      def commitResult = sh returnStdout: true, script: "$curlAuth $githubApiCommitUrl".trim()
+      def commits = jsonSlurper.parseText(commitResult)
+
+      echo "COMMITS: $commits"
+
 
       // FIXME: use github API to check for only repos with a recent commit and only run truffleHog on these
       // Keep a record of the commit SHAs
 
       // The truffleHog docker run cause exit code 1 which fails the build so need the || true to ignore it
-      def truffleHogCmd = "docker run dxa4481/trufflehog --json --regex https://github.com/${it}.git || true"
-      def truffleHogRes = sh returnStdout: true, script: truffleHogCmd
-      def reportRes = []
-      def jsonSlurper = new JsonSlurper()
-      def secretsFound = false
+      // def truffleHogCmd = "docker run dxa4481/trufflehog --json --regex https://github.com/${it}.git || true"
+      // def truffleHogRes = sh returnStdout: true, script: truffleHogCmd
+      // def reportRes = []
+      // def jsonSlurper = new JsonSlurper()
+      // def secretsFound = false
 
-      truffleHogRes.trim().split('\n').each {
-        def result = jsonSlurper.parseText(it)
-        def dateObj = new Date().parse("yyyy-MM-dd HH:mm:ss", result.date)
+      // truffleHogRes.trim().split('\n').each {
+      //   def result = jsonSlurper.parseText(it)
+      //   def dateObj = new Date().parse("yyyy-MM-dd HH:mm:ss", result.date)
 
-        // FIXME: match by commit SHA instead of date when we use the github API to get only recent commits
-        if (dateObj > commitCheckDate) {
-          def message = "Reason: $result.reason\n" +
-                        "Date: $result.date\n" +
-                        "Branch: $result.branch\n" +
-                        "Hash: $result.commitHash\n" +
-                        "Filepath: $result.path\n" +
-                        "StringsFound: $result.stringsFound\n" +
-                        "Commit: $result.commit"
-          print message
-          secretsFound = true
-        }
-      }
+      //   // FIXME: match by commit SHA instead of date when we use the github API to get only recent commits
+      //   if (dateObj > commitCheckDate) {
+      //     def message = "Reason: $result.reason\n" +
+      //                   "Date: $result.date\n" +
+      //                   "Branch: $result.branch\n" +
+      //                   "Hash: $result.commitHash\n" +
+      //                   "Filepath: $result.path\n" +
+      //                   "StringsFound: $result.stringsFound\n" +
+      //                   "Commit: $result.commit"
+      //     print message
+      //     secretsFound = true
+      //   }
+      // }
 
-      try {
-        if (secretsFound) {
-          def msg = """POTENTIAL SECRETS DETECTED
-          ${JOB_NAME}/${BUILD_NUMBER}
-          (<${BUILD_URL}|Open>)"""
-          def channel = "#secretdetection"
+      // try {
+      //   if (secretsFound) {
+      //     def msg = """POTENTIAL SECRETS DETECTED
+      //     ${JOB_NAME}/${BUILD_NUMBER}
+      //     (<${BUILD_URL}|Open>)"""
+      //     def channel = "#secretdetection"
 
-          slackSend channel: channel,
-                    color: "#ff0000",
-                    message: msg.replace("  ", "")
-        }
-      } catch (e) { }
+      //     slackSend channel: channel,
+      //               color: "#ff0000",
+      //               message: msg.replace("  ", "")
+      //   }
+      // } catch (e) { }
 
       echo "Finished scanning $it"
     }
