6cca450ccc0a5a0dfa207b9e4cd7cf354320f0bc
Paul Andrews <paul.andrews@defra.gov.uk>
2020-04-28 21:02:14 +0100

    get branch commits

--- vars/secretScanner.groovy
+++ vars/secretScanner.groovy
@@ -60,7 +60,7 @@ def scanWithinWindow(githubUser, repositoryPrefix, scanWindowHrs) {
 
       branches.each {
         def githubApiCommitUrl = "https://api.github.com/repos/$repo/commits?since=$commitCheckDate\\&sha=${it.name}"
-        // def commitResult = sh returnStdout: true, script: "$curlAuth $githubApiCommitUrl".trim()
+        def commitResult = sh returnStdout: true, script: "$curlAuth $githubApiCommitUrl".trim()
         // // jsonSlurper = new JsonSlurper()
         // def commits = jsonSlurper.parseText(commitResult)
 
