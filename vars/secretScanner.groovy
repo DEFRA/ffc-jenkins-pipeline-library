e0807d609f09a3fb17e3f964381a0cb6db3c3dc5
Paul Andrews <paul.andrews@defra.gov.uk>
2020-05-01 22:34:06 +0100

    can be null

--- vars/secretScanner.groovy
+++ vars/secretScanner.groovy
@@ -18,7 +18,7 @@ def runTruffleHog(dockerImgName, repoName, commitShas=null) {
     if (it.length() == 0) return  // readJSON won't accept an empty string
     def result = readJSON text: it
 
-    if (commitShas.contains(result.commitHash) || !commitShas) {
+    if (!commitShas || commitShas.contains(result.commitHash)) {
       def message = "Reason: $result.reason\n" +
                     "Date: $result.date\n" +
                     "Repo: $repoName\n" +
