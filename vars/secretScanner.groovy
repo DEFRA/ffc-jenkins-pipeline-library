7ca554085bc5c2b26c6618e0b8381af1fec5acfd
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2020-05-07 10:37:10 +0100

    Update secret message output to link to github commit URL (#77)
    Update secret message output to link to github commit and file URL

--- vars/secretScanner.groovy
+++ vars/secretScanner.groovy
@@ -26,7 +26,9 @@ def runTruffleHog(dockerImgName, repoName, excludeStrings, commitShas=null) {
                       "Branch: $result.branch\n" +
                       "Hash: $result.commitHash\n" +
                       "File path: $result.path\n" +
-                      "Strings found: $result.stringsFound\n"
+                      "Strings found: $result.stringsFound\n" +
+                      "Commit link: https://github.com/$repoName/commit/$result.commitHash\n" +
+                      "File link: https://github.com/$repoName/blob/$result.commitHash/$result.path\n"
 
         secretMessages.add(message)
       }
