d2f01b58230d1b8aa44c0a21c6dea36435760b2b
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-05-19 14:19:51 +0100

    Use class for helm script

--- vars/helm.groovy
+++ vars/helm.groovy
@@ -1,69 +1,25 @@
 import uk.gov.defra.ffc.Helm
 
 def getExtraCommands(tag) {
-  return "--set labels.version=$tag --install --atomic --version=$tag"
+  return Helm.getExtraCommands(tag)
 }
 
 def getPrCommands(registry, chartName, tag) {
-  def helmValues = [
-    /image=$registry\/$chartName:$tag/,
-    /namespace=$chartName-$tag/,
-    /pr=$tag/,
-    /deployment.redeployOnChange=$tag-$BUILD_NUMBER/
-    ].join(',')
-
-    return "--set $helmValues"
+  return Helm.getPrCommands(registry, chartName, tag, BUILD_NUMBER)
 }
 
 def deployChart(environment, registry, chartName, tag) {
-  withKubeConfig([credentialsId: "kubeconfig-$environment"]) {
-    withCredentials([
-      file(credentialsId: "$chartName-$environment-values", variable: 'envValues'),
-      file(credentialsId: "$chartName-pr-values", variable: 'prValues')
-    ]) {
-      def deploymentName = "$chartName-$tag"
-      def extraCommands = getExtraCommands(tag)
-      def prCommands = getPrCommands(registry, chartName, tag)
-      sh("kubectl get namespaces $deploymentName || kubectl create namespace $deploymentName")
-      sh("helm upgrade $deploymentName --namespace=$deploymentName ./helm/$chartName -f $envValues -f $prValues $prCommands $extraCommands")
-      Helm.writeUrlIfIngress(this, deploymentName)
-    }
-  }
+  Helm.deployChart(this, environment, registry, chartName, tag)
 }
 
 def undeployChart(environment, chartName, tag) {
-  def deploymentName = "$chartName-$tag"
-  echo "removing deployment $deploymentName"
-  withKubeConfig([credentialsId: "kubeconfig-$environment"]) {
-    sh("helm uninstall $deploymentName -n $deploymentName || echo error removing deployment $deploymentName")
-    sh("kubectl delete namespaces $deploymentName || echo error removing namespace $deploymentName")
-  }
+  Helm.undeployChart(this, environment, chartName, tag)
 }
 
 def publishChart(registry, chartName, tag) {
-  withCredentials([
-    usernamePassword(credentialsId: 'artifactory-credentials', usernameVariable: 'username', passwordVariable: 'password')
-  ]) {
-    // jenkins doesn't tidy up folder, remove old charts before running
-    sh('rm -rf helm-charts')
-    dir('helm-charts') {
-      sh("sed -i -e 's/image: .*/image: $registry\\/$chartName:$tag/' ../helm/$chartName/values.yaml")
-      Helm.addHelmRepo(this, 'ffc-public', HELM_CHART_REPO_PUBLIC)
-      sh("helm package ../helm/$chartName --version $tag --dependency-update")
-      sh("curl -u $username:$password -X PUT ${ARTIFACTORY_REPO_URL}ffc-helm-local/$chartName-${tag}.tgz -T $chartName-${tag}.tgz")
-    }
-  }
+  Helm.publishChart(this, registry, chartName, tag)
 }
 
 def deployRemoteChart(environment, namespace, chartName, chartVersion) {
-  withKubeConfig([credentialsId: "kubeconfig-$environment"]) {
-    withCredentials([
-      file(credentialsId: "$chartName-$environment-values", variable: 'values')
-    ]) {
-      def extraCommands = getExtraCommands(chartVersion)
-      Helm.addHelmRepo(this, 'ffc', "${ARTIFACTORY_REPO_URL}ffc-helm-virtual")
-      sh("kubectl get namespaces $namespace || kubectl create namespace $namespace")
-      sh("helm upgrade --namespace=$namespace $chartName -f $values --set namespace=$namespace ffc/$chartName $extraCommands")
-    }
-  }
+  Helm.deployRemoteChart(this, environment, namespace, chartName, chartVersion)
 }
