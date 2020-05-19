0cbf573ac94b272aea5d7f6dad549e10b0688339
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-05-19 08:26:01 +0100

    Complete move to using Helm class

--- src/uk/gov/defra/ffc/Helm.groovy
+++ src/uk/gov/defra/ffc/Helm.groovy
@@ -5,4 +5,8 @@ class Helm implements Serializable {
     ctx.sh("kubectl get ingress -n $deploymentName -o json --ignore-not-found | jq '.items[0].spec.rules[0].host // empty' | xargs --no-run-if-empty printf 'Build available for review at https://%s\n'")
   }
 
+  static def addHelmRepo(ctx, repoName, url) {
+    ctx.sh("helm repo add $repoName $url")
+    ctx.sh("helm repo update")
+  }
 }
--- vars/helm.groovy
+++ vars/helm.groovy
@@ -1,3 +1,5 @@
+import uk.gov.defra.ffc.Helm
+
 def getExtraCommands(tag) {
   return "--set labels.version=$tag --install --atomic --version=$tag"
 }
@@ -22,55 +24,47 @@ def deployChart(environment, registry, chartName, tag) {
       def deploymentName = "$chartName-$tag"
       def extraCommands = getExtraCommands(tag)
       def prCommands = getPrCommands(registry, chartName, tag)
-      sh "kubectl get namespaces $deploymentName || kubectl create namespace $deploymentName"
-      sh "helm upgrade $deploymentName --namespace=$deploymentName ./helm/$chartName -f $envValues -f $prValues $prCommands $extraCommands"
+      sh("kubectl get namespaces $deploymentName || kubectl create namespace $deploymentName")
+      sh("helm upgrade $deploymentName --namespace=$deploymentName ./helm/$chartName -f $envValues -f $prValues $prCommands $extraCommands")
       Helm.writeUrlIfIngress(this, deploymentName)
     }
   }
 }
 
-// public
 def undeployChart(environment, chartName, tag) {
   def deploymentName = "$chartName-$tag"
   echo "removing deployment $deploymentName"
   withKubeConfig([credentialsId: "kubeconfig-$environment"]) {
-    sh "helm uninstall $deploymentName -n $deploymentName || echo error removing deployment $deploymentName"
-    sh "kubectl delete namespaces $deploymentName || echo error removing namespace $deploymentName"
+    sh("helm uninstall $deploymentName -n $deploymentName || echo error removing deployment $deploymentName")
+    sh("kubectl delete namespaces $deploymentName || echo error removing namespace $deploymentName")
   }
 }
 
-// public
 def publishChart(registry, chartName, tag) {
   withCredentials([
     usernamePassword(credentialsId: 'artifactory-credentials', usernameVariable: 'username', passwordVariable: 'password')
   ]) {
     // jenkins doesn't tidy up folder, remove old charts before running
-    sh "rm -rf helm-charts"
+    sh("rm -rf helm-charts")
     dir('helm-charts') {
-      sh "sed -i -e 's/image: .*/image: $registry\\/$chartName:$tag/' ../helm/$chartName/values.yaml"
-      addHelmRepo('ffc-public', HELM_CHART_REPO_PUBLIC)
-      sh "helm package ../helm/$chartName --version $tag --dependency-update"
-      sh "curl -u $username:$password -X PUT ${ARTIFACTORY_REPO_URL}ffc-helm-local/$chartName-${tag}.tgz -T $chartName-${tag}.tgz"
+      sh("sed -i -e 's/image: .*/image: $registry\\/$chartName:$tag/' ../helm/$chartName/values.yaml")
+      Helm.addHelmRepo(this, 'ffc-public', HELM_CHART_REPO_PUBLIC)
+      sh("helm package ../helm/$chartName --version $tag --dependency-update")
+      sh("curl -u $username:$password -X PUT ${ARTIFACTORY_REPO_URL}ffc-helm-local/$chartName-${tag}.tgz -T $chartName-${tag}.tgz")
     }
   }
 }
 
-// public
 def deployRemoteChart(environment, namespace, chartName, chartVersion) {
   withKubeConfig([credentialsId: "kubeconfig-$environment"]) {
     withCredentials([
       file(credentialsId: "$chartName-$environment-values", variable: 'values')
     ]) {
       def extraCommands = getExtraCommands(chartVersion)
-      addHelmRepo('ffc', "${ARTIFACTORY_REPO_URL}ffc-helm-virtual")
-      sh "kubectl get namespaces $namespace || kubectl create namespace $namespace"
-      sh "helm upgrade --namespace=$namespace $chartName -f $values --set namespace=$namespace ffc/$chartName $extraCommands"
+      Helm.addHelmRepo(this, 'ffc', "${ARTIFACTORY_REPO_URL}ffc-helm-virtual")
+      sh("kubectl get namespaces $namespace || kubectl create namespace $namespace")
+      sh("helm upgrade --namespace=$namespace $chartName -f $values --set namespace=$namespace ffc/$chartName $extraCommands")
     }
   }
 }
-
-// private
-def addHelmRepo(repoName, url) {
-  sh "helm repo add $repoName $url"
-  sh "helm repo update"
 }
