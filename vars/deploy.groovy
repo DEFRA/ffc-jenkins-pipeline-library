d80567c259e5bb67192bc1f38bbfb57036bf17cb
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-05-19 14:08:22 +0100

    Use class for deploy script

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,11 +1,5 @@
-import uk.gov.defra.ffc.Utils
+import uk.gov.defra.ffc.Deploy
 
 def trigger(jenkinsUrl, repoName, token, params) {
-  def folder = Utils.getFolder(repoName)
-  def url = "$jenkinsUrl/job/$folder/job/$repoName-deploy/buildWithParameters?token=$token"
-  params.each { param ->
-    url = url + "\\&$param.key=$param.value"
-  }
-  echo("Triggering deployment for $url")
-  sh(script: "curl -fk $url")
+  Deploy.trigger(this, jenkinsUrl, repoName, token, params)
 }
