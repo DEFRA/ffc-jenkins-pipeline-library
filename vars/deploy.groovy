e31a050c28ad56953076c55c5d80ea1e4e26cc83
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-05-19 09:02:28 +0100

    Use parens consistently

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,12 +1,11 @@
 import uk.gov.defra.ffc.Utils
 
-// public
 def trigger(jenkinsUrl, repoName, token, params) {
   def folder = Utils.getFolder(repoName)
   def url = "$jenkinsUrl/job/$folder/job/$repoName-deploy/buildWithParameters?token=$token"
   params.each { param ->
     url = url + "\\&$param.key=$param.value"
   }
-  echo "Triggering deployment for $url"
+  echo("Triggering deployment for $url")
   sh(script: "curl -fk $url")
 }
