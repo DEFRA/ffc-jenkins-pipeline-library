47a008cdebaa05c5c8c53f201cbe4a02d9f44aca
Mark Harrop <mark.harrop@defra.gov.uk>
2020-05-19 09:18:35 +0100

    Update documentation

--- vars/cleanup.groovy
+++ vars/cleanup.groovy
@@ -1,22 +1,22 @@
 def call(Map config=[:]) {
   node {
-    stage('delete k8s resources if PR closed') {
-      cleanupKubernetes(config.environment)
+    stage('delete resources of closed PR') {
+      cleanup(config.environment)
     }
   }
 }
 
-def cleanupKubernetes(environment) {
+def cleanup(environment) {
   if (repoName == '' || SOURCE_PROJECT_NAME == '') {
-    echo "Unable to determine repo name and branch name, k8s cleanup cancelled"
+    echo "Unable to determine repo name and branch name, cleanup cancelled"
   } else {
     def apiParams = "state=closed&sort=updated&direction=desc&head=DEFRA:$SOURCE_PROJECT_NAME"
     def apiUrl = "https://api.github.com/repos/DEFRA/$repoName/pulls?$apiParams"
-    def closedPrNo = sh(returnStdout: true, script: "curl '$apiUrl' | jq '.[].number'").trim()
+    def closedPrNo = sh(returnStdout: true, script: "curl '$apiUrl' | jq 'first | .number'").trim()
     if (closedPrNo == '') {
-      echo "Could not find closed PR for branch $SOURCE_PROJECT_NAME of $repoName, k8s cleanup cancelled"
+      echo "Could not find closed PR for branch $SOURCE_PROJECT_NAME of $repoName, cleanup cancelled"
     }else {
-      echo "Tidying up k8s resources for PR $closedPrNo of $repoName after branch $SOURCE_PROJECT_NAME deleted"
+      echo "Tidying up kubernetes resources for PR $closedPrNo of $repoName after branch $SOURCE_PROJECT_NAME deleted"
       helm.undeployChart(environment, repoName, "pr$closedPrNo")
     }
   }
