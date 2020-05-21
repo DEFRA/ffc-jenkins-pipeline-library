92c3a1985acf4ef50b80b7bcecc459e14508dc7a
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-05-21 14:44:33 +0100

    Move existing methods into class

--- src/uk/gov/defra/ffc/SecretScanner.groovy
+++ src/uk/gov/defra/ffc/SecretScanner.groovy
@@ -1,5 +1,7 @@
 package uk.gov.defra.ffc
 
+import uk.gov.defra.ffc.Notifications
+
 class SecretScanner implements Serializable {
   private static def runTruffleHog(ctx, dockerImgName, repoName, excludeStrings, commitShas=null) {
     // truffleHog seems to alway exit with code 1 even though it appears to run fine
@@ -32,11 +34,69 @@ class SecretScanner implements Serializable {
     return secretMessages
   }
 
+  private static def getMatchingRepos(ctx, curlAuth, githubOwner, repositoryPrefix) {
+    def githubReposUrl = "https://api.github.com/users/$githubOwner/repos?per_page=100"
+
+    def curlHeaders = ctx.sh(returnStdout: true, script: "$curlAuth --head $githubReposUrl")
+    def numPages = SecretScanner.getNumPages(ctx, curlHeaders)
+
+    ctx.echo("Number of pages of repos: $numPages")
+
+    def matchingRepos = []
+    def matchStr = "$githubOwner/$repositoryPrefix".toLowerCase()
+
+    (numPages as Integer).times { page ->
+      def reposResults = ctx.sh(returnStdout: true, script: "$curlAuth $githubReposUrl\\&page=${page+1}")
+      def allRepos = ctx.readJSON(text: reposResults)
+
+      allRepos.each { repo ->
+        if (repo.full_name.toLowerCase().startsWith(matchStr)) {
+          matchingRepos.add(repo.full_name)
+        }
+      }
+    }
+
+    ctx.echo("Matching repos: $matchingRepos")
+
+    return matchingRepos
+  }
+
+  private static def getNumPages(ctx, curlHeaders) {
+    def linkHeader = curlHeaders.split('\n').find { header -> header.toLowerCase().startsWith('link:') }
+
+    if (linkHeader) {
+      def lastLink = linkHeader.split(',').find { link -> link.contains('rel="last"') }
+
+      if (lastLink) {
+        return lastLink[(lastLink.lastIndexOf('&page=')+6)..(lastLink.lastIndexOf('>')-1)]
+      }
+    }
+
+    return "1"
+  }
+
+  private static def reportSecrets(ctx, secretMessages, repo, channel) {
+    secretMessages.each { ctx.echo("$it") }
+
+    if (channel.length() == 0) return
+
+    def message = "POTENTIAL SECRETS DETECTED IN $repo\n$JOB_NAME/$BUILD_NUMBER\n(<$BUILD_URL|Open>)"
+
+    Notifications.sendMessage(ctx, channel, message, true)
+  }
+
+  @NonCPS // Don't run this in the Jenkins sandbox so that use (groovy.time.TimeCategory) will work
+  private static def getCommitCheckDate(scanWindowHrs) {
+    use (groovy.time.TimeCategory) {
+      return (new Date() - scanWindowHrs.hours - 10.minutes).format("yyyy-MM-dd'T'HH:mm:ss'Z'", TimeZone.getTimeZone('UTC'))
+    }
+  }
+
   static def scanWithinWindow(ctx, githubCredentialId, dockerImgName, githubOwner, repositoryPrefix, scanWindowHrs, excludeStrings, slackChannel="") {
     ctx.withCredentials([ctx.string(credentialsId: githubCredentialId, variable: 'githubToken')]) {
       def curlAuth = "curl --header 'Authorization: token $ctx.githubToken' --silent"
 
-      def matchingRepos = getMatchingRepos(curlAuth, githubOwner, repositoryPrefix)
+      def matchingRepos = SecretScanner.getMatchingRepos(ctx, curlAuth, githubOwner, repositoryPrefix)
       def commitCheckDate = getCommitCheckDate(scanWindowHrs)
 
       ctx.echo("Commit check date: $commitCheckDate")
@@ -55,7 +115,7 @@ class SecretScanner implements Serializable {
         branches.each { branch ->
           def githubCommitUrl = "https://api.github.com/repos/$repo/commits?since=$commitCheckDate\\&sha=${branch.name}\\&per_page=100"
           def curlHeaders = ctx.sh(returnStdout: true, script: "$curlAuth --head $githubCommitUrl")
-          def numPages = getNumPages(curlHeaders)
+          def numPages = SecretScanner.getNumPages(ctx, curlHeaders)
 
           (numPages as Integer).times { page ->
             def commitResults = ctx.sh(returnStdout: true, script: "$curlAuth $githubCommitUrl\\&page=${page+1}")
@@ -69,8 +129,32 @@ class SecretScanner implements Serializable {
 
           if (!secretMessages.isEmpty()) {
             secretsFound = true
-            reportSecrets(secretMessages, repo, slackChannel)
+            SecretScanner.reportSecrets(ctx, secretMessages, repo, slackChannel)
           }
+        }
+
+        ctx.echo("Finished scanning $repo")
+      }
+
+      return secretsFound
+    }
+  }
+
+  static def scanFullHistory(ctx, githubCredentialId, dockerImgName, githubOwner, repositoryPrefix, excludeStrings, slackChannel="") {
+    ctx.withCredentials([ctx.string(credentialsId: githubCredentialId, variable: 'githubToken')]) {
+      def curlAuth = "curl --header 'Authorization: token $ctx.githubToken' --silent"
+      def matchingRepos = SecretScanner.getMatchingRepos(ctx, curlAuth, githubOwner, repositoryPrefix)
+
+      def secretsFound = false
+
+      matchingRepos.each { repo ->
+        ctx.echo("Scanning $repo")
+
+        def secretMessages = SecretScanner.runTruffleHog(ctx, dockerImgName, repo, excludeStrings)
+
+        if (!secretMessages.isEmpty()) {
+          secretsFound = true
+          SecretScanner.reportSecrets(ctx, secretMessages, repo, slackChannel)
         }
 
         ctx.echo("Finished scanning $repo")
--- vars/secretScanner.groovy
+++ vars/secretScanner.groovy
@@ -1,97 +1,9 @@
 import uk.gov.defra.ffc.SecretScanner
 
-// private
-@NonCPS // Don't run this in the Jenkins sandbox so that use (groovy.time.TimeCategory) will work
-def getCommitCheckDate(scanWindowHrs) {
-  use (groovy.time.TimeCategory) {
-    return (new Date() - scanWindowHrs.hours - 10.minutes).format("yyyy-MM-dd'T'HH:mm:ss'Z'", TimeZone.getTimeZone('UTC'))
-  }
-}
-
-// private
-def runTruffleHog(dockerImgName, repoName, excludeStrings, commitShas=null) {
-  return SecretScanner.runTruffleHog(this, dockerImgName, repoName, excludeStrings, commitShas=null)
-}
-
-// private
-def getNumPages(curlHeaders) {
-  def linkHeader = curlHeaders.split('\n').find { header -> header.toLowerCase().startsWith('link:') }
-
-  if (linkHeader) {
-    def lastLink = linkHeader.split(',').find { link -> link.contains('rel="last"') }
-
-    if (lastLink) {
-      return lastLink[(lastLink.lastIndexOf('&page=')+6)..(lastLink.lastIndexOf('>')-1)]
-    }
-  }
-
-  return "1"
-}
-
-// private
-def getMatchingRepos(curlAuth, githubOwner, repositoryPrefix) {
-  def githubReposUrl = "https://api.github.com/users/$githubOwner/repos?per_page=100"
-
-  def curlHeaders = sh returnStdout: true, script: "$curlAuth --head $githubReposUrl"
-  def numPages = getNumPages(curlHeaders)
-
-  echo "Number of pages of repos: $numPages"
-
-  def matchingRepos = []
-  def matchStr = "$githubOwner/$repositoryPrefix".toLowerCase()
-
-  (numPages as Integer).times { page ->
-    def reposResults = sh returnStdout: true, script: "$curlAuth $githubReposUrl\\&page=${page+1}"
-    def allRepos = readJSON text: reposResults
-
-    allRepos.each { repo ->
-      if (repo.full_name.toLowerCase().startsWith(matchStr)) {
-        matchingRepos.add(repo.full_name)
-      }
-    }
-  }
-
-  echo "Matching repos: $matchingRepos"
-
-  return matchingRepos
-}
-
-// private
-def reportSecrets(secretMessages, repo, channel) {
-  secretMessages.each { echo "$it" }
-
-  if (channel.length() == 0) return
-
-  def message = "POTENTIAL SECRETS DETECTED IN $repo\n$JOB_NAME/$BUILD_NUMBER\n(<$BUILD_URL|Open>)"
-
-  notifySlack.sendMessage(channel, message, true)
-}
-
 def scanWithinWindow(githubCredentialId, dockerImgName, githubOwner, repositoryPrefix, scanWindowHrs, excludeStrings, slackChannel="") {
   return SecretScanner.scanWithinWindow(this, githubCredentialId, dockerImgName, githubOwner, repositoryPrefix, scanWindowHrs, excludeStrings, slackChannel)
 }
 
-// public
 def scanFullHistory(githubCredentialId, dockerImgName, githubOwner, repositoryPrefix, excludeStrings, slackChannel="") {
-  withCredentials([string(credentialsId: githubCredentialId, variable: 'githubToken')]) {
-    def curlAuth = "curl --header 'Authorization: token $githubToken' --silent"
-    def matchingRepos = getMatchingRepos(curlAuth, githubOwner, repositoryPrefix)
-
-    def secretsFound = false
-
-    matchingRepos.each { repo ->
-      echo "Scanning $repo"
-
-      def secretMessages = runTruffleHog(dockerImgName, repo, excludeStrings)
-
-      if (!secretMessages.isEmpty()) {
-        secretsFound = true
-        reportSecrets(secretMessages, repo, slackChannel)
-      }
-
-      echo "Finished scanning $repo"
-    }
-
-    return secretsFound
-  }
+  return SecretScanner.scanFullHistory(this, githubCredentialId, dockerImgName, githubOwner, repositoryPrefix, excludeStrings, slackChannel)
 }
