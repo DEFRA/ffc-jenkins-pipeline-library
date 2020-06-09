ac634e622bdf7df69a6f97ed58dc3d0ed159458b
Paul Andrews <paul.andrews@defra.gov.uk>
2020-06-09 22:15:09 +0100

    Just get username

--- src/uk/gov/defra/ffc/Helm.groovy
+++ src/uk/gov/defra/ffc/Helm.groovy
@@ -36,7 +36,7 @@ class Helm implements Serializable {
         def prCommands = Helm.getPrCommands(registry, chartName, tag, ctx.BUILD_NUMBER)
 
         def yamlFile = "postgresConfig.yaml"
-        ctx.sh("az appconfig kv export --connection-string \"$ctx.appConfigConnectionString\" -d file --path $yamlFile --key \"postgresService.postgresUsername\" --resolve-keyvault --format yaml --yes")
+        ctx.sh("az appconfig kv export --connection-string \"$ctx.appConfigConnectionString\" -d file --path $yamlFile --key \"postgresUsername\" --resolve-keyvault --format yaml --yes")
         ctx.sh("cat $yamlFile")
 
         ctx.sh("kubectl get namespaces $deploymentName || kubectl create namespace $deploymentName")
