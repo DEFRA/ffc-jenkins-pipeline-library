eca22673a62d9c10ffc78b2816cf9ec5eea80ec7
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-06-12 14:04:07 +0100

    improvement: use single quotes for no interpolation

--- vars/helm.groovy
+++ vars/helm.groovy
@@ -34,7 +34,7 @@ def publishChart(registry, chartName, tag, helmChartRepoType="artifactory", over
   }
 }
 
-def deployRemoteChart(environment, namespace, chartName, chartVersion, helmChartRepoType="artifactory") {
+def deployRemoteChart(environment, namespace, chartName, chartVersion, helmChartRepoType='artifactory') {
   if (helmChartRepoType) {
     switch (helmChartRepoType.toLowerCase()) {
       case 'artifactory':
