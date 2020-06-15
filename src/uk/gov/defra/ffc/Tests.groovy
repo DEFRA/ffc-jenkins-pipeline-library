bf79e956073a2485708325c62c6cd0c54b07770e
John Watson <johnwatson484@hotmail.com>
2020-06-15 14:21:53 +0100

    Default sonar params updated

--- src/uk/gov/defra/ffc/Tests.groovy
+++ src/uk/gov/defra/ffc/Tests.groovy
@@ -43,17 +43,15 @@ class Tests implements Serializable {
 
   static def buildCodeAnalysisDefaultParams(projectName, branch, pr) {
     return [
-    'sonar.projectKey' : projectName,
+    'sonar.organization': 'defra'
+    'sonar.projectKey': projectName,
     'sonar.sources': '.',
     'sonar.pullrequest.base': 'master',
     'sonar.pullrequest.branch': branch,
     'sonar.pullrequest.key': pr,
     'sonar.pullrequest.provider': 'GitHub',
-    'sonar.pullrequest.github.repository': "defra/${projectName}"
-    // 'sonar.javascript.exclusions': '**/jest.config.js,**/__mocks__/**,**/node_modules/**,**/test/**,**/test-output/**',
-    // 'sonar.javascript.lcov.reportPaths': 'test-output/lcov.info',
-    // 'sonar.exclusions': '/test/**,**/*.test.js',
-    // 'sonar.language': 'js'
+    'sonar.pullrequest.github.repository': "defra/${projectName}",
+    'sonar.language': 'js'
     ];
   }
 
