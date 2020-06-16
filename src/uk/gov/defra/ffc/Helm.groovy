2f77821db9d3f76044f0406f4248e62478081f06
Paul Andrews <paul.andrews@defra.gov.uk>
2020-06-16 15:19:46 +0100

    typo

--- src/uk/gov/defra/ffc/Helm.groovy
+++ src/uk/gov/defra/ffc/Helm.groovy
@@ -28,7 +28,7 @@ class Helm implements Serializable {
     def configItems = [:]
     def suppressConsoleOutput = '#!/bin/bash +x\n'
 
-    for ( int i = 0 ; i < configKeys.size ; i++ )
+    for ( int i = 0 ; i < configKeys.size ; i++ ) {
       def key = configKeys[i]
       def appConfigResults = ctx.sh(returnStdout: true, script:"$suppressConsoleOutput az appconfig kv list --subscription \$APP_CONFIG_SUBSCRIPTION --name \$APP_CONFIG_NAME --key $prefix$delimiter$key --label $label --resolve-keyvault").trim()
 
