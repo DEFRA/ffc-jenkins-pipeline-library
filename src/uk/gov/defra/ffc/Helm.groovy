a80ea5871b2282cc7668431cb1e3378eb33081c3
Paul Andrews <paul.andrews@defra.gov.uk>
2020-06-17 23:46:19 +0100

    test output

--- src/uk/gov/defra/ffc/Helm.groovy
+++ src/uk/gov/defra/ffc/Helm.groovy
@@ -34,6 +34,9 @@ class Helm implements Serializable {
       def appConfigResults = ctx.sh(returnStdout: true, script:"$suppressConsoleOutput az appconfig kv list --subscription \$APP_CONFIG_SUBSCRIPTION --name \$APP_CONFIG_NAME --key $prefix$delimiter$key --label $label --resolve-keyvault | jq -r '.[] | .value'").trim()
       def numResults = appConfigResults.tokenize('\n').size()
 
+      // FIXME: NEEDS TESTING
+      echo "NUMBER OF RESULTS FOUND: $numResults"
+
       if (numResults == 1) {
           configItems[key] = appConfigResults
       }
