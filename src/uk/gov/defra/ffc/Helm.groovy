d14cdb5ba91f0416e304311c9f66ad3ff9d781f2
Paul Andrews <paul.andrews@defra.gov.uk>
2020-06-18 09:22:36 +0100

    test3

--- src/uk/gov/defra/ffc/Helm.groovy
+++ src/uk/gov/defra/ffc/Helm.groovy
@@ -77,7 +77,7 @@ class Helm implements Serializable {
       def chartyName = "./helm/$chartName"
 
       def appConfigResults = ctx.sh(returnStdout: true, script:"$suppressConsoleOutput az appconfig kv list --subscription \$APP_CONFIG_SUBSCRIPTION --name \$APP_CONFIG_NAME --key dev/post.username --label \\\\0 --resolve-keyvault | jq -r '.[] | .value'").trim()
-      def qqq = appConfigResults.replaceAll(/,/,/\,/)
+      def qqq = appConfigResults.replaceAll($/,/$,$/\,/$)
       def myStr = $/"$qqq"/$
       ctx.sh($/$suppressConsoleOutput helm upgrade $deploymentName --namespace=$deploymentName $chartyName --set post.username=$myStr $prCommands $extraCommands/$)
       Helm.writeUrlIfIngress(ctx, deploymentName)
