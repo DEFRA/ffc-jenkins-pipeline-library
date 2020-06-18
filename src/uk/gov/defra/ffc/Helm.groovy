24e5e504d5872afaa5b4ff8284c10b72859d3e41
Paul Andrews <paul.andrews@defra.gov.uk>
2020-06-18 10:31:22 +0100

    test more

--- src/uk/gov/defra/ffc/Helm.groovy
+++ src/uk/gov/defra/ffc/Helm.groovy
@@ -77,8 +77,8 @@ class Helm implements Serializable {
       def chartyName = "./helm/$chartName"
 
       def appConfigResults = ctx.sh(returnStdout: true, script:"$suppressConsoleOutput az appconfig kv list --subscription \$APP_CONFIG_SUBSCRIPTION --name \$APP_CONFIG_NAME --key dev/post.username --label \\\\0 --resolve-keyvault | jq -r '.[] | .value'").trim()
-      // appConfigResults = appConfigResults.replaceAll('{', 'P') // Need to do the backslash replacing first as we are adding them below!
-      appConfigResults = appConfigResults.replaceAll(/,/, /\\,/)
+      // appConfigResults = appConfigResults.replaceAll(/{/, /qqq/) // Need to do the backslash replacing first as we are adding them below!
+      appConfigResults = appConfigResults.replaceAll(',', '\\,')
       appConfigResults = appConfigResults.replaceAll(/"/, /\\"/)
       appConfigResults = appConfigResults.replaceAll(/`/, /\\`/)
       def myStr = $/"$appConfigResults"/$
