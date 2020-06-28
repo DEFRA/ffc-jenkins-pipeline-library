e26c70a2eece8d7d8bd1bfd5221b5fefc64e4581
Paul Andrews <paul.andrews@defra.gov.uk>
2020-06-28 16:17:50 +0100

    Fix typo

--- vars/queues.groovy
+++ vars/queues.groovy
@@ -1,7 +1,7 @@
 import uk.gov.defra.ffc.Terraform
 
 def destroyPrSqsQueues(repoName, prCode) {
-  Terraform.destroyPrSqsQueues(this, repoName, prCode) {
+  Terraform.destroyPrSqsQueues(this, repoName, prCode)
 }
 
 def provisionPrSqsQueue(repoName, prCode, queuePurpose, serviceCode, serviceName, serviceType) {
