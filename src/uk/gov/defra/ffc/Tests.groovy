5b7d1409517a51c7e99584df7b2439b32f36f227
John Watson <johnwatson484@hotmail.com>
2020-07-01 15:47:08 +0100

    Pull added

--- src/uk/gov/defra/ffc/Tests.groovy
+++ src/uk/gov/defra/ffc/Tests.groovy
@@ -50,7 +50,7 @@ class Tests implements Serializable {
         args = args + " -e $param.key=$param.value"
       }
 
-      ctx.sh("docker run -v \$(pwd)/$project/:/home/dotnet/project -e SONAR_TOKEN=$ctx.token $args defradigital/ffc-dotnet-core-sonar")
+      ctx.sh("docker run -v \$(pwd)/$project/:/home/dotnet/project -e SONAR_TOKEN=$ctx.token $args --pull defradigital/ffc-dotnet-core-sonar")
     }
   }
 
