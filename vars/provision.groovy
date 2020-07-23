e63fba78d827e53db96df2a5a81614e4855dada5
Mark Harrop <mark.harrop@defra.gov.uk>
2020-07-23 13:38:45 +0100

    fix namespace name

--- vars/provision.groovy
+++ vars/provision.groovy
@@ -10,7 +10,7 @@ def readManifest() {
 }
 
 def listQueues(resourceGroup, namespace, prefix) {
-  def resGroupAndNamespace = "--resource-group $resourceGroup --namespace-name $nameSpace"
+  def resGroupAndNamespace = "--resource-group $resourceGroup --namespace-name $namespace"
   def jqCommand = "jq -r '.[]| select(.name | startswith(\"$prefix\")) | .name')"
   def script = "az servicebus queue list $resGroupAndNamespace | $jqCommand"
   def queueNames = sh(returnStdout: true, script: script).trim()
