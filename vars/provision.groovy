a2258c48aec3308b4b10fc3d4bb1792f460ad46e
Fay Toward <fay.toward@rpa.gov.uk>
2020-07-23 16:27:40 +0100

    Manifest and create queue added

--- vars/provision.groovy
+++ vars/provision.groovy
@@ -1,48 +1,51 @@
+provisionFilePath = 'provision.azure.yaml'
+
 def createResources(repoName, pr) {
-  if(manifest.azure) {
-    deletePrResources(repoName, pr)   
-    createAllResources(pr)
+  if(fileExists(provisionFilePath)) {
+    //deletePrResources(repoName, pr)   
+    createAllResources(repoName, pr)
   }
 }
 
-def readManifest() {
-  return manifest
+def fileExists(filePath){
+  return sh("test -f $filePath && echo true || echo false")
 }
 
-def deletePrResources(repoName, pr) {
-  deleteQueues(repoName, pr)
+def readManifest(resource) {
+ return sh("yq r $provisionFilePath resources.$resource.**")
 }
 
-def deleteBuildQueues(repoName, pr) {
-  deleteQueues(repoName, pr)
+// def deletePrResources(repoName, pr) {
+//   deleteQueues(repoName, pr)
+// }
+
+// def deleteBuildQueues(repoName, pr) {
+//   deleteQueues(repoName, pr)
+// }
+
+def createAllResources(repoName, pr) {
+  createQueues(readManifest('queues'), repoName, pr)
 }
 
-def createAllResources(pr) {
-  def manifest = readManifest()
-  if(manifest.queues) {
-    createQueues(manifest.queues, pr)
-  }
-}
+// def deleteQueues(repoName, pr) {
+//   def resourceGroup = AZURE_SERVICE_BUS_RESOURCE_GROUP
+//   def namespace = AZURE_SERVICE_BUS_NAMESPACE
+//   def queues = sh("az servicebus queue list --resource-group $resourceGroup --namespace-name $namespace | jq. // TODO get PR queues that match convention")
+//   // az servicebus queue list --resource-group SNDFFCINFRG1001 --namespace-name SNDFFCINFSB1001  | jq -r '.[]| select(.name | startswith("mh")) | .name'
+//   foreach(queues in queues) {
+//     sh('az servicebus queue delete')
+//   }
+// }
 
-def deleteQueues(repoName, pr) {
-  def resourceGroup = AZURE_SERVICE_BUS_RESOURCE_GROUP
-  def namespace = AZURE_SERVICE_BUS_NAMESPACE
-  def queues = sh("az servicebus queue list --resource-group $resourceGroup --namespace-name $namespace | jq. // TODO get PR queues that match convention")
-  // az servicebus queue list --resource-group SNDFFCINFRG1001 --namespace-name SNDFFCINFSB1001  | jq -r '.[]| select(.name | startswith("mh")) | .name'
-  foreach(queues in queues) {
-    sh('az servicebus queue delete')
-  }
-}
-
-def createQueues(queues, pr) {
-  foreach(queue in queues) {
-    if(pr) {
-      createQueue(pr queue name)
+def createQueues(queues, repoName, pr) {
+  queues.each { key ->
+    if(pr != '') {
+      createQueue("$repoName-pr$pr-$key")
     }
-    createQueue(build queue name)
+    createQueue("$repoName-$BUILD_NUMBER-$pr-$key")
   }
 }
 
-def createQueue(queue) {
-  sh("az servicebus queue create --name $queue")
+def createQueue(queueName) {
+  sh("az servicebus queue create --resource-group $AZURE_SERVICE_BUS_RESOURCE_GROUP --namespace-name $AZURE_SERVICE_BUS_NAMESPACE --name $queue")
 }
