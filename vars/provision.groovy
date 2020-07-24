ad23f8fb04ec381c325d3c32bee6bfd26d7a2adf
Mark Harrop <markharrop@yahoo.com>
2020-07-24 10:39:23 +0100

    List queues by prefix (#147)
    * List queues by prefix
    
    https://eaflood.atlassian.net/browse/PSD-902
    
    branch to merge into psd-902-add-dynamic-queue to add listing queues by
    prefix
    
    * add listQueues function
    
    Co-authored-by: Seymourito Pattisson <seymour.pattisson@defra.gov.uk>
    Co-authored-by: Mark Harrop <mark.harrop@defra.gov.uk>

--- vars/provision.groovy
+++ vars/provision.groovy
@@ -9,12 +9,25 @@ def readManifest() {
   return manifest
 }
 
+def validateQueueName(name) {
+  assert name ==~ /^[A-Za-z0-9]$|^[A-Za-z0-9][\w-\.\/\~]*[A-Za-z0-9]$/ : "Invalid queue name: '$name'"
+}
+
+def listQueues(prefix) {
+  validateQueueName(prefix)
+  def resGroupAndNamespace = "--resource-group $AZURE_SERVICE_BUS_RESOURCE_GROUP --namespace-name $AZURE_SERVICE_BUS_NAMESPACE"
+  def jqCommand = "jq -r '.[]| select(.name | startswith(\"$prefix\")) | .name'"
+  def script = "az servicebus queue list $resGroupAndNamespace | $jqCommand"
+  def queueNames = sh(returnStdout: true, script: script).trim()
+  return queueNames.tokenize('\n')
+}
+
 def deletePrResources(repoName, pr) {
-  deleteQueues(repoName, pr)
+  deleteQueues(repoName + pr)
 }
 
 def deleteBuildQueues(repoName, pr) {
-  deleteQueues(repoName, pr)
+  deleteQueues(repoName + pr)
 }
 
 def createAllResources(pr) {
@@ -24,17 +37,14 @@ def createAllResources(pr) {
   }
 }
 
-def deleteQueues(repoName, pr) {
-  def resourceGroup = AZURE_SERVICE_BUS_RESOURCE_GROUP
-  def namespace = AZURE_SERVICE_BUS_NAMESPACE
-  def queues = sh("az servicebus queue list --resource-group $resourceGroup --namespace-name $namespace | jq. // TODO get PR queues that match convention")
-  // az servicebus queue list --resource-group SNDFFCINFRG1001 --namespace-name SNDFFCINFSB1001  | jq -r '.[]| select(.name | startswith("mh")) | .name'
-  foreach(queues in queues) {
-    sh('az servicebus queue delete')
+def deleteQueues(prefix) {
+  def queues = listQueues(prefix)
+  queues.each {
+    sh("az servicebus queue delete --resource-group $AZURE_SERVICE_BUS_RESOURCE_GROUP --namespace-name $AZURE_SERVICE_BUS_NAMESPACE --name $it")
   }
 }
 
-def createQueues(queues, pr) {
+/* def createQueues(queues, pr) {
   foreach(queue in queues) {
     if(pr) {
       createQueue(pr queue name)
@@ -42,7 +52,7 @@ def createQueues(queues, pr) {
     createQueue(build queue name)
   }
 }
-
+ */
 def createQueue(queue) {
   sh("az servicebus queue create --name $queue")
 }
