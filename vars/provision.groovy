d9aedf6f70d97c6821dcc61fe1067e4b5e43f9ad
John Watson <johnwatson484@hotmail.com>
2020-07-24 11:22:01 +0100

    Update from branch

--- vars/provision.groovy
+++ vars/provision.groovy
@@ -7,6 +7,26 @@ def createResources(repoName, pr) {
   }
 }
 
+def deleteQueues(prefix) {
+  def queues = listQueues(prefix)
+  queues.each {
+    sh("az servicebus queue delete --resource-group $AZURE_SERVICE_BUS_RESOURCE_GROUP --namespace-name $AZURE_SERVICE_BUS_NAMESPACE --name $it")
+  }
+}
+
+def validateQueueName(name) {
+  assert name ==~ /^[A-Za-z0-9]$|^[A-Za-z0-9][\w-\.\/\~]*[A-Za-z0-9]$/ : "Invalid queue name: '$name'"
+}
+
+def listQueues(prefix) {
+  validateQueueName(prefix)
+  def resGroupAndNamespace = "--resource-group $AZURE_SERVICE_BUS_RESOURCE_GROUP --namespace-name $AZURE_SERVICE_BUS_NAMESPACE"
+  def jqCommand = "jq -r '.[]| select(.name | startswith(\"$prefix\")) | .name'"
+  def script = "az servicebus queue list $resGroupAndNamespace | $jqCommand"
+  def queueNames = sh(returnStdout: true, script: script).trim()
+  return queueNames.tokenize('\n')
+}
+
 def readManifest(filePath, resource) {
  def resources = sh(returnStdout: true, script: "yq r $filePath resources.$resource.**").trim()
  return resources.tokenize('\n')
