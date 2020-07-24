81546a98a107bcca72e5268a658d1e1b8e2a5ad1
John Watson <johnwatson484@hotmail.com>
2020-07-24 15:49:29 +0100

    Refactored to classes

--- vars/provision.groovy
+++ vars/provision.groovy
@@ -1,77 +1,9 @@
+import uk.gov.defra.ffc.Provision
+
 def createResources(repoName, pr) {
-  def filePath = 'provision.azure.yaml'
-  if(fileExists(filePath)) {
-    deletePrResources(repoName, pr)   
-    createAllResources(filePath, repoName, pr)
-  }
+  Provision.createResources(this, repoName, pr)
 }
 
 def deleteBuildResources(repoName, pr) {
-  deleteQueues(getBuildQueuePrefix(repoName, pr))
+  Provision.deleteQueues(this, repoName, pr))
 }
-
-def createAllResources(filePath, repoName, pr) {
-  def queues = readManifest(filePath, 'queues')
-  createQueues(queues, repoName, pr)
-}
-
-def createQueues(queues, repoName, pr) {
-  createBuildQueues(queues, repoName, pr)
-  if(pr != '') {
-    createPrQueues(queues, repoName, pr)
-  }
-}
-
-def createPrQueues(queues, repoName, pr) {
-  queues.each {
-    createQueue("$repoName-pr$pr-$it")
-  }
-}
-
-def createBuildQueues(queues, repoName, pr) {
-  queues.each {
-    createQueue("${getBuildQueuePrefix(repoName, pr)}$it")
-  }
-}
-
-def createQueue(queueName) {
-  validateQueueName(queueName)  
-  def azCommand = 'az servicebus queue create'
-  sh("$azCommand ${getResGroupAndNamespace()} --name $queueName --max-size 1024")
-}
-
-def deletePrResources(repoName, pr) {
-  deleteQueues("$repoName-pr$pr-")
-}
-
-def deleteQueues(prefix) {
-  def queues = listQueues(prefix)
-  queues.each {
-    sh("az servicebus queue delete ${getResGroupAndNamespace()} --name $it")
-  }
-}
-
-def validateQueueName(name) {
-  assert name ==~ /^[A-Za-z0-9]$|^[A-Za-z0-9][\w-\.\/\~]*[A-Za-z0-9]$/ : "Invalid queue name: '$name'"
-}
-
-def readManifest(filePath, resource) {
- def resources = sh(returnStdout: true, script: "yq r $filePath resources.${resource}.*.name").trim()
- return resources.tokenize('\n')
-}
-
-def getResGroupAndNamespace () {
-  return "--resource-group $AZURE_SERVICE_BUS_RESOURCE_GROUP --namespace-name $AZURE_SERVICE_BUS_NAMESPACE"
-}
-
-def getBuildQueuePrefix (repoName, pr) {
-  return "$repoName-build$BUILD_NUMBER-$pr-"
-}
-
-def listQueues(prefix) {
-  def jqCommand = "jq -r '.[]| select(.name | startswith(\"$prefix\")) | .name'"
-  def script = "az servicebus queue list ${getResGroupAndNamespace()} | $jqCommand"
-  def queueNames = sh(returnStdout: true, script: script).trim()
-  return queueNames.tokenize('\n')
-}
-
