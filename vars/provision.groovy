a6c430bd3d891249b1b20dd756104f5bd9022091
Fay Toward <fay.toward@rpa.gov.uk>
2020-07-24 10:53:55 +0100

    comments removed

--- vars/provision.groovy
+++ vars/provision.groovy
@@ -2,7 +2,7 @@
 def createResources(repoName, pr) {
   def filePath = 'provision.azure.yaml'
   if(fileExists(filePath)) {
-    //deletePrResources(repoName, pr)   
+    deletePrResources(repoName, pr)   
     createAllResources(filePath, repoName, pr)
   }
 }
@@ -12,28 +12,10 @@ def readManifest(filePath, resource) {
  return resources.tokenize('\n')
 }
 
-// def deletePrResources(repoName, pr) {
-//   deleteQueues(repoName, pr)
-// }
-
-// def deleteBuildQueues(repoName, pr) {
-//   deleteQueues(repoName, pr)
-// }
-
 def createAllResources(filePath, repoName, pr) {
   def queues = readManifest(filePath, 'queues')
   createQueues(queues, repoName, pr)
 }
-
-// def deleteQueues(repoName, pr) {
-//   def resourceGroup = AZURE_SERVICE_BUS_RESOURCE_GROUP
-//   def namespace = AZURE_SERVICE_BUS_NAMESPACE
-//   def queues = sh("az servicebus queue list --resource-group $resourceGroup --namespace-name $namespace | jq. // TODO get PR queues that match convention")
-//   // az servicebus queue list --resource-group SNDFFCINFRG1001 --namespace-name SNDFFCINFSB1001  | jq -r '.[]| select(.name | startswith("mh")) | .name'
-//   foreach(queues in queues) {
-//     sh('az servicebus queue delete')
-//   }
-// }
 
 def createQueues(queues, repoName, pr) {
   queues.each { key ->
