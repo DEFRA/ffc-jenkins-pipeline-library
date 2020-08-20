e412a9a3ca376646f7a631d42acb15182f26d0f6
Fay Toward <fay.toward@rpa.gov.uk>
2020-08-20 11:56:53 +0100

    pact test

---
+++ src/uk/gov/defra/ffc/Pact.groovy
@@ -0,0 +1,22 @@
+package uk.gov.defra.ffc
+
+class Provision implements Serializable {
+
+def string(credentialsId,variable){
+  Pact.string(this, credentialsId, variable)
+}
+
+def usernamePassword(credentialsId, usernameVariable, passwordVariable){
+  Pact.usernamePassword(this, credentialsId, usernameVariable, passwordVariable)
+}
+
+def publishPacts(repoName, string, usernamePassword){
+  findFiles glob: "*.json"
+  echo "Found ${pacts.size()} pact file(s) to publish"
+  for (publishPact in publishPacts) {
+    def provider = pact.name.substring("$repoName-".length(), pact.name.indexOf(".json"))
+    echo "Publishing ${pact.name} to broker"
+    sh "curl -k -v -XPUT -H \"Content-Type: application/json\" --user $pactUsername:$pactPassword -d@${pact.name} $pactBrokerURL/pacts/provider/$provider/consumer/$repoName/version/$version+$commitSha"
+  }
+}
+}
--- vars/pact.groovy
+++ vars/pact.groovy
@@ -9,12 +9,6 @@ def usernamePassword(credentialsId, usernameVariable, passwordVariable){
 }
 
 def publishPacts(repoName, string, usernamePassword){
-  findFiles glob: "*.json"
-  echo "Found ${pacts.size()} pact file(s) to publish"
-  for (publishPact in publishPacts) {
-    def provider = pact.name.substring("$repoName-".length(), pact.name.indexOf(".json"))
-    echo "Publishing ${pact.name} to broker"
-    sh "curl -k -v -XPUT -H \"Content-Type: application/json\" --user $pactUsername:$pactPassword -d@${pact.name} $pactBrokerURL/pacts/provider/$provider/consumer/$repoName/version/$version+$commitSha"
-  }
+  Pact.publishPacts(this, repoName, string, usernamePassword)
 }
 
