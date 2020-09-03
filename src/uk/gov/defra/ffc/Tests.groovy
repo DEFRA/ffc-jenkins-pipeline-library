7bac1d1f6ebd83dbe25c715bd026413d836fd929
Fay Toward <fay.toward@rpa.gov.uk>
2020-09-03 10:31:37 +0100

    parametered acceptance tests

--- src/uk/gov/defra/ffc/Tests.groovy
+++ src/uk/gov/defra/ffc/Tests.groovy
@@ -139,9 +139,9 @@ class Tests implements Serializable {
           ctx.withEnv(["TEST_ENVIRONMENT_ROOT_URL=https://${hostname}.ffc.snd.azure.defra.cloud"]) {
           ctx.sh('docker-compose run wdio-cucumber')
           }
+          }
         } finally {
-          ctx.sh('docker-compose down -v')
-        
+          ctx.sh('docker-compose down -v')        
       }
     } else {
       ctx.echo('No "/test/acceptance/docker-compose.yaml" found therefore skipping this step.')
