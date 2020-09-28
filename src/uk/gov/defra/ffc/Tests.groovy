997b18b50cdbdf7f6ce24afa621e7aae69695e19
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-09-28 11:56:46 +0100

    feat: ensure latest version of image is pulled and used

--- src/uk/gov/defra/ffc/Tests.groovy
+++ src/uk/gov/defra/ffc/Tests.groovy
@@ -85,7 +85,9 @@ class Tests implements Serializable {
       params.each { param ->
         args = args + " -e $param.key=$param.value"
       }
-      ctx.sh("docker run -v \$(pwd)/:/home/dotnet/project -e SONAR_TOKEN=$ctx.token $args defradigital/ffc-dotnet-core-sonar")
+      def sonarImage = 'defradigital/ffc-dotnet-core-sonar'
+      ctx.sh("docker rmi $sonarImage")
+      ctx.sh("docker run -v \$(pwd)/:/home/dotnet/project -e SONAR_TOKEN=$ctx.token $args $sonarImage")
     }
   }
 
