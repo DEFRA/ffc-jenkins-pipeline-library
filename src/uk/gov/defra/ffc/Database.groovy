9a8fa04a2cd4934623fa16be6e0f8d97a6cd893a
John Watson <johnwatson484@hotmail.com>
2020-10-12 13:43:49 +0100

    Permission updated

--- src/uk/gov/defra/ffc/Database.groovy
+++ src/uk/gov/defra/ffc/Database.groovy
@@ -66,7 +66,7 @@ class Database implements Serializable {
 
   static runRemoteMigrations(ctx, environment, repoName, version) {
     ctx.sh("mkdir -p -m 777 release-files")
-    ctx.sh("ll")
+    ctx.sh("ls -la")
     ctx.dir("release-files") {
       ctx.sh("wget https://api.github.com/repos/defra/${repoName}/tarball/${version} -O release")
       ctx.sh("tar -xvf release")
