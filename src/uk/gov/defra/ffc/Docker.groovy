d7129a717e72b53d898347f13682b1925d92b316
Fay Toward <fay.toward@rpa.gov.uk>
2020-10-29 11:05:56 +0000

    spacing

--- src/uk/gov/defra/ffc/Docker.groovy
+++ src/uk/gov/defra/ffc/Docker.groovy
@@ -3,15 +3,15 @@ package uk.gov.defra.ffc
 class Docker implements Serializable {
   static def buildTestImage(ctx, credentialsId, registry, projectName, buildNumber, tag) {    
    ctx.docker.withRegistry("https://$registry", credentialsId) {
-     ctx.sh("docker-compose -p $projectName-$tag-$buildNumber -f docker-compose.yaml -f docker-compose.test.yaml build")
+    ctx.sh("docker-compose -p $projectName-$tag-$buildNumber -f docker-compose.yaml -f docker-compose.test.yaml build")
    }
   }
 
   static def buildAndPushContainerImage(ctx, credentialsId, registry, imageName, tag) {
     ctx.docker.withRegistry("https://$registry", credentialsId) {
-    ctx.sh('docker-compose -f docker-compose.yaml build')
-    ctx.sh("docker tag $imageName $registry/$imageName:$tag")
-    ctx.sh("docker push $registry/$imageName:$tag")
+      ctx.sh('docker-compose -f docker-compose.yaml build')
+      ctx.sh("docker tag $imageName $registry/$imageName:$tag")
+      ctx.sh("docker push $registry/$imageName:$tag")
     }
   }
 }
