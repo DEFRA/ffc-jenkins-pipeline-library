299c4f649fa85894bd82021a0694726cf6888557
John Watson <johnwatson484@hotmail.com>
2020-10-29 13:19:57 +0000

    Add temp remove

--- src/uk/gov/defra/ffc/Build.groovy
+++ src/uk/gov/defra/ffc/Build.groovy
@@ -28,7 +28,7 @@ class Build implements Serializable {
   static def checkoutSourceCode(ctx, defaultBranch) {
     ctx.checkout(ctx.scm)
     ctx.sh("git remote set-branches --add origin ${defaultBranch}")
-    ctx.sh("git remote remove main")
+    ctx.sh("git remote remove origin/main")
     ctx.sh("git fetch")
   }
 
