463031d3cc08a567f4beb8390580fd877b143678
Fay Toward <fay.toward@rpa.gov.uk>
2020-11-12 09:36:26 +0000

    build file changes

--- vars/build.groovy
+++ vars/build.groovy
@@ -2,38 +2,38 @@ import uk.gov.defra.ffc.Build
 import uk.gov.defra.ffc.Docker
 import uk.gov.defra.ffc.Tests
 
-def getVariables(version, defaultBranch) {
+def getVariables(String version, String defaultBranch) {
   return Build.getVariables(this, version, defaultBranch)
 }
 
-def getDefaultBranch(defaultBranch, requestedBranch) {
+def getDefaultBranch(String defaultBranch, String requestedBranch) {
   return Build.getDefaultBranch(defaultBranch, requestedBranch)
 }
 
-def checkoutSourceCode(defaultBranch) {
+def checkoutSourceCode(String defaultBranch) {
   return Build.checkoutSourceCode(this, defaultBranch)
 }
 
-def buildTestImage(credentialsId, registry, projectName, buildNumber, tag) {
+def buildTestImage(String credentialsId, String registry, String projectName, String buildNumber, String tag) {
   Docker.buildTestImage(this, credentialsId, registry, projectName, buildNumber, tag)
 }
 
-def runTests(projectName, serviceName, buildNumber, tag, pr, environment) {
+def runTests(String projectName, String serviceName, String buildNumber, String tag, int pr, String environment) {
   Tests.runTests(this, projectName, serviceName, buildNumber, tag, pr, environment)
 }
 
-def buildAndPushContainerImage(credentialsId, registry, imageName, tag) {
+def buildAndPushContainerImage(String credentialsId, String registry, String imageName, String tag) {
   Docker.buildAndPushContainerImage(this, credentialsId, registry, imageName, tag)
 }
 
-def npmAudit(auditLevel, logType, failOnIssues, containerImage, containerWorkDir, pr) {
+def npmAudit(String auditLevel, String logType, String failOnIssues, String containerImage, String containerWorkDir, int pr) {
   Build.npmAudit(this, auditLevel, logType, failOnIssues, containerImage, containerWorkDir, pr)
 }
 
-def extractSynkFiles(projectName, buildNumber, tag) {
+def extractSynkFiles(String projectName, int buildNumber, String tag) {
   Build.extractSynkFiles(this, projectName, buildNumber, tag)
 }
 
-def snykTest(failOnIssues, organisation, severity, targetFile = '', pr) {
+def snykTest(String failOnIssues, String organisation, String severity, String targetFile = '', String pr) {
   Build.snykTest(this, failOnIssues, organisation, severity, targetFile, pr)
 }
