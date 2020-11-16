e4b5352ef8b13506ddc36707dcd4bdc19bd779ee
Fay Toward <fay.toward@rpa.gov.uk>
2020-11-16 15:27:24 +0000

    database file changes

--- vars/database.groovy
+++ vars/database.groovy
@@ -1,13 +1,13 @@
 import uk.gov.defra.ffc.Database
 
-def provisionPrDbRoleAndSchema(host, dbName, jenkinsUserCredId, prUserCredId, prCode, useIfNotExists=false) {
+def provisionPrDbRoleAndSchema(String host, String dbName, String jenkinsUserCredId, String prUserCredId, String prCode, Boolean useIfNotExists=false) {
   return Database.provisionPrDbRoleAndSchema(this, host, dbName, jenkinsUserCredId, prUserCredId, prCode, useIfNotExists)
 }
 
-def destroyPrDbRoleAndSchema(host, dbName, jenkinsUserCredId, prCode) {
+def destroyPrDbRoleAndSchema(String host, String dbName, String jenkinsUserCredId, String prCode) {
   return Database.destroyPrDbRoleAndSchema(this, host, dbName, jenkinsUserCredId, prCode)
 }
 
-def runRemoteMigrations(environment, repoName, version) {
+def runRemoteMigrations(String environment, String repoName, String version) {
   Database.runRemoteMigrations(this, environment, repoName, version)
 }
