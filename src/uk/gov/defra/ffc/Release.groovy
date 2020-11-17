8e70103b578a7e86af2838a83ef4b18f77838796
fareedmohammed <fareed.mohammed.uk@gmail.com>
2020-11-17 18:19:59 +0000

    feat: more changes 23

--- src/uk/gov/defra/ffc/Release.groovy
+++ src/uk/gov/defra/ffc/Release.groovy
@@ -74,7 +74,10 @@ class Release implements Serializable {
 
     ctx.echo("Triggering release $versionTag for $repoName")
     boolean result = false
-    result = ctx.sh(returnStdout: true, script: "curl -s -X POST -H 'Authorization: token $token' -d '{ \"tag_name\" : \"$versionTag\", \"name\" : \"Release $versionTag\", \"body\" : \" test release description\" }' https://api.github.com/repos/DEFRA/$repoName/releases")
+
+    String releaseDescriptionTrimmed = releaseDescription.replaceAll("(^[\\r\\n]+|[\\r\\n]+$)", "")
+
+    result = ctx.sh(returnStdout: true, script: "curl -s -X POST -H 'Authorization: token $token' -d '{ \"tag_name\" : \"$versionTag\", \"name\" : \"Release $versionTag\", \"body\" : \" $releaseDescriptionTrimmed \" }' https://api.github.com/repos/DEFRA/$repoName/releases")
     ctx.echo("The release result is $result")
 
     if (exists(ctx, versionTag, repoName, token)) {
