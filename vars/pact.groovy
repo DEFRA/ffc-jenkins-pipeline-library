310821da1a8c98b7c8b968e200cbf7c92d0c527a
Fay Toward <fay.toward@rpa.gov.uk>
2020-11-18 11:08:20 +0000

    pact file changes

--- vars/pact.groovy
+++ vars/pact.groovy
@@ -1,5 +1,5 @@
 import uk.gov.defra.ffc.Pact
 
-def publishContractsToPactBroker(repoName, version, commitSha) {
+def publishContractsToPactBroker(String repoName, String version, String commitSha) {
   Pact.publishContractsToPactBroker(this, repoName, version, commitSha)
 }
