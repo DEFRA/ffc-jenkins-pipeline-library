dffd21e72d341e13375998b608276209c13606fb
Fay Toward <fay.toward@rpa.gov.uk>
2020-11-18 13:13:26 +0000

    pact file changes

--- vars/provision.groovy
+++ vars/provision.groovy
@@ -1,9 +1,9 @@
 import uk.gov.defra.ffc.Provision
 
-def createResources(environment, repoName, pr) {
+def createResources(String environment, String repoName, String pr) {
   Provision.createResources(this, environment, repoName, pr)
 }
 
-def deleteBuildResources(repoName, pr) {
+def deleteBuildResources(String repoName, String pr) {
   Provision.deleteBuildResources(this, repoName, pr)
 }
