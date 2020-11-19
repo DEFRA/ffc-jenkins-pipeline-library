c1bbcb637bf81b20bf6c63e7b398de9d16a80633
Fay Toward <fay.toward@rpa.gov.uk>
2020-11-19 11:28:58 +0000

    test

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,10 +1,10 @@
 import uk.gov.defra.ffc.Tests
 
-def lintHelm(chartName) {
+def lintHelm(String chartName) {
   Tests.lintHelm(this, chartName)
 }
 
-def runGitHubSuperLinter(disableErrors=false) {
+def runGitHubSuperLinter(Boolean disableErrors=false) {
   Tests.runGitHubSuperLinter(this, disableErrors)
 }
 
@@ -12,35 +12,35 @@ def createJUnitReport() {
   Tests.createJUnitReport(this)
 }
 
-def deleteOutput(containerImage, containerWorkDir) {
+def deleteOutput(String containerImage, String containerWorkDir) {
   Tests.deleteOutput(this, containerImage, containerWorkDir)
 }
 
-def analyseNodeJsCode(sonarQubeEnv, sonarScanner, repoName, branch, defaultBranch, pr) {
+def analyseNodeJsCode(String sonarQubeEnv, String sonarScanner, String repoName, String branch, String defaultBranch, String pr) {
   Tests.analyseNodeJsCode(this, sonarQubeEnv, sonarScanner, buildCodeAnalysisNodeJsParams(repoName, branch, defaultBranch, pr))
 }
 
-def analyseDotNetCode(repoName, branch, defaultBranch, pr) {
+def analyseDotNetCode(String repoName, String branch, String defaultBranch, String pr) {
   Tests.analyseDotNetCode(this, buildCodeAnalysisDotNetParams(repoName, branch, defaultBranch, pr))
 }
 
-def buildCodeAnalysisNodeJsParams(projectName, branch, defaultBranch, pr) {
+def buildCodeAnalysisNodeJsParams(String projectName, String branch, String defaultBranch, String pr) {
   return Tests.buildCodeAnalysisNodeJsParams(projectName, branch, defaultBranch, pr)
 }
 
-def buildCodeAnalysisDotNetParams(projectName, branch, defaultBranch, pr) {
+def buildCodeAnalysisDotNetParams(String projectName, String branch, String defaultBranch, String pr) {
   return Tests.buildCodeAnalysisDotNetParams(projectName, branch, defaultBranch, pr)
 }
 
-def runAcceptanceTests(pr, environment, repoName) {
+def runAcceptanceTests(String pr, String environment, String repoName) {
   Tests.runAcceptanceTests(this, pr, environment, repoName)
 }
 
-def runZapScan(projectName, buildNumber, tag) {
+def runZapScan(String projectName, String buildNumber, String tag) {
   Tests.runZapScan(this, projectName, buildNumber, tag)
 }
 
 
-def runPa11y(projectName, buildNumber, tag) {
+def runPa11y(String projectName, String buildNumber, String tag) {
   Tests.runPa11y(this, projectName, buildNumber, tag)
 }
