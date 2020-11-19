3a742ec9690c5bb24159a3a5799934c5f2cfc181
Fay Toward <fay.toward@rpa.gov.uk>
2020-11-19 16:42:06 +0000

    test

--- vars/secretScanner.groovy
+++ vars/secretScanner.groovy
@@ -1,9 +1,9 @@
 import uk.gov.defra.ffc.SecretScanner
 
-def scanWithinWindow(String githubCredentialId, String dockerImgName, String githubOwner, String repositoryPrefix, Date() scanWindowHrs, String excludeStrings, String slackChannel="") {
+def scanWithinWindow(String githubCredentialId, String dockerImgName, String githubOwner, String repositoryPrefix, Date() scanWindowHrs, String[] excludeStrings, String slackChannel="") {
   return SecretScanner.scanWithinWindow(this, githubCredentialId, dockerImgName, githubOwner, repositoryPrefix, scanWindowHrs, excludeStrings, slackChannel)
 }
 
-def scanFullHistory(String githubCredentialId, String dockerImgName, String githubOwner, String repositoryPrefix, String excludeStrings, String slackChannel="") {
+def scanFullHistory(String githubCredentialId, String dockerImgName, String githubOwner, String repositoryPrefix, String[] excludeStrings, String slackChannel="") {
   return SecretScanner.scanFullHistory(this, githubCredentialId, dockerImgName, githubOwner, repositoryPrefix, excludeStrings, slackChannel)
 }
