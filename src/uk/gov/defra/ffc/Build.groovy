f73a996cba901d061b2b7623b11a8edcfd7bef2e
Fay Toward <fay.toward@rpa.gov.uk>
2020-11-19 15:46:31 +0000

    test

--- src/uk/gov/defra/ffc/Build.groovy
+++ src/uk/gov/defra/ffc/Build.groovy
@@ -78,7 +78,7 @@ class Build implements Serializable {
     }
   }
 
-  static def extractSynkFiles(ctx, projectName, buildNumber.toInteger(), tag) {
+  static def extractSynkFiles(ctx, projectName, buildNumber, tag) {
     try {
       ctx.sh("docker-compose -p $projectName-$tag-$buildNumber -f docker-compose.snyk.yaml up")
     } finally {
--- vars/buildDotNetCore.groovy
+++ vars/buildDotNetCore.groovy
@@ -44,7 +44,7 @@ def call(Map config=[:]) {
        stage('Snyk test') {
          // ensure obj folder exists and is writable by all
          sh("chmod 777 ${config.project}/obj || mkdir -p -m 777 ${config.project}/obj")
-         build.extractSynkFiles(repoName, BUILD_NUMBER.toInteger(), tag)
+         build.extractSynkFiles(repoName, BUILD_NUMBER, tag)
          build.snykTest(config.snykFailOnIssues, config.snykOrganisation, config.snykSeverity, "${config.project}.sln", pr)
        }
      }
