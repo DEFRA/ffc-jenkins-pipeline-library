c1fd4f01e3866eafe594c5a58191e7995cbc254b
Fay Toward <fay.toward@rpa.gov.uk>
2020-11-20 10:15:35 +0000

    test

--- vars/provision.groovy
+++ vars/provision.groovy
@@ -1,9 +1,9 @@
 import uk.gov.defra.ffc.Provision
 
-def createResources(String environment, String repoName, String pr) {
+void createResources(String environment, String repoName, String pr) {
   Provision.createResources(this, environment, repoName, pr)
 }
 
-def deleteBuildResources(String repoName, String pr) {
+void deleteBuildResources(String repoName, String pr) {
   Provision.deleteBuildResources(this, repoName, pr)
 }
