e65df6c32844d6202d027277fb758cd96a54befe
Fay Toward <fay.toward@rpa.gov.uk>
2020-11-20 09:22:32 +0000

    test

--- vars/build.groovy
+++ vars/build.groovy
@@ -2,38 +2,38 @@ import uk.gov.defra.ffc.Build
 import uk.gov.defra.ffc.Docker
 import uk.gov.defra.ffc.Tests
 
-def getVariables(String version, String defaultBranch) {
+String [] getVariables(String version, String defaultBranch) {
   return Build.getVariables(this, version, defaultBranch)
 }
 
-def getDefaultBranch(String defaultBranch, String requestedBranch) {
+String getDefaultBranch(String defaultBranch, String requestedBranch) {
   return Build.getDefaultBranch(defaultBranch, requestedBranch)
 }
 
-def checkoutSourceCode(String defaultBranch) {
+String checkoutSourceCode(String defaultBranch) {
   return Build.checkoutSourceCode(this, defaultBranch)
 }
 
-def buildTestImage(String credentialsId, String registry, String projectName, String buildNumber, String tag) {
+void buildTestImage(String credentialsId, String registry, String projectName, String buildNumber, String tag) {
   Docker.buildTestImage(this, credentialsId, registry, projectName, buildNumber, tag)
 }
 
-def runTests(String projectName, String serviceName, String buildNumber, String tag, String pr, String environment) {
+void runTests(String projectName, String serviceName, String buildNumber, String tag, String pr, String environment) {
   Tests.runTests(this, projectName, serviceName, buildNumber, tag, pr, environment)
 }
 
-def buildAndPushContainerImage(String credentialsId, String registry, String imageName, String tag) {
+void buildAndPushContainerImage(String credentialsId, String registry, String imageName, String tag) {
   Docker.buildAndPushContainerImage(this, credentialsId, registry, imageName, tag)
 }
 
-def npmAudit(String auditLevel, String logType, Boolean failOnIssues, String containerImage, String containerWorkDir, String pr) {
+void npmAudit(String auditLevel, String logType, Boolean failOnIssues, String containerImage, String containerWorkDir, String pr) {
   Build.npmAudit(this, auditLevel, logType, failOnIssues, containerImage, containerWorkDir, pr)
 }
 
-def extractSynkFiles(String projectName, String buildNumber, String tag) {
+void extractSynkFiles(String projectName, String buildNumber, String tag) {
   Build.extractSynkFiles(this, projectName, buildNumber, tag)
 }
 
-def snykTest(Boolean failOnIssues, String organisation, String severity, String targetFile = '', String pr) {
+void snykTest(Boolean failOnIssues, String organisation, String severity, String targetFile = '', String pr) {
   Build.snykTest(this, failOnIssues, organisation, severity, targetFile, pr)
 }
