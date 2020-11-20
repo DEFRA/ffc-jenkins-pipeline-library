fe1fa081441ac23cf5a32c891ca1eab8c6b375e7
Fay Toward <fay.toward@rpa.gov.uk>
2020-11-20 10:16:09 +0000

    test

--- vars/release.groovy
+++ vars/release.groovy
@@ -1,9 +1,9 @@
 import uk.gov.defra.ffc.Release
 
-def trigger(String versionTag, String repoName, String releaseDescription, String token) {
+String trigger(String versionTag, String repoName, String releaseDescription, String token) {
   return Release.trigger(this, versionTag, repoName, releaseDescription, token)
 }
 
-def addSemverTags(String version, String repoName) {
+void addSemverTags(String version, String repoName) {
   Release.addSemverTags(this, version, repoName)
 }
