9a4cc8e3870daa03b51c68909a84ee8ee9920976
Fay Toward <fay.toward@rpa.gov.uk>
2020-11-20 13:59:06 +0000

    test

--- vars/release.groovy
+++ vars/release.groovy
@@ -1,6 +1,6 @@
 import uk.gov.defra.ffc.Release
 
-String trigger(String versionTag, String repoName, String releaseDescription, String token) {
+Boolean trigger(String versionTag, String repoName, String releaseDescription, String token) {
   return Release.trigger(this, versionTag, repoName, releaseDescription, token)
 }
 
--- vars/secretScanner.groovy
+++ vars/secretScanner.groovy
@@ -1,9 +1,9 @@
 import uk.gov.defra.ffc.SecretScanner
 
-String scanWithinWindow(String githubCredentialId, String dockerImgName, String githubOwner, String repositoryPrefix, Date() scanWindowHrs, String[] excludeStrings, String slackChannel="") {
+Boolean scanWithinWindow(String githubCredentialId, String dockerImgName, String githubOwner, String repositoryPrefix, Date() scanWindowHrs, String[] excludeStrings, String slackChannel="") {
   return SecretScanner.scanWithinWindow(this, githubCredentialId, dockerImgName, githubOwner, repositoryPrefix, scanWindowHrs, excludeStrings, slackChannel)
 }
 
-String scanFullHistory(String githubCredentialId, String dockerImgName, String githubOwner, String repositoryPrefix, String[] excludeStrings, String slackChannel="") {
+Boolean scanFullHistory(String githubCredentialId, String dockerImgName, String githubOwner, String repositoryPrefix, String[] excludeStrings, String slackChannel="") {
   return SecretScanner.scanFullHistory(this, githubCredentialId, dockerImgName, githubOwner, repositoryPrefix, excludeStrings, slackChannel)
 }
