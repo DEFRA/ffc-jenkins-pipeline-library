a8436a188b278b4da76f0145b96afc810c14aa30
John Watson <johnwatson484@hotmail.com>
2020-11-23 14:11:42 +0000

    Fix Array parameters

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,5 +1,5 @@
 import uk.gov.defra.ffc.Deploy
 
-void trigger(String jenkinsUrl, String repoName, String token, String[] params) {
+void trigger(String jenkinsUrl, String repoName, String token, def params) {
   Deploy.trigger(this, jenkinsUrl, repoName, token, params)
 }
