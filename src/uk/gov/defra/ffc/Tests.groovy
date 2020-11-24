3021554ef31d53995aaaba833d3dad80e75ffb90
John Watson <johnwatson484@hotmail.com>
2020-11-24 15:48:57 +0000

    Specify folders to change

--- src/uk/gov/defra/ffc/Tests.groovy
+++ src/uk/gov/defra/ffc/Tests.groovy
@@ -77,7 +77,8 @@ class Tests implements Serializable {
 
   static def deleteOutput(ctx, containerImage, containerWorkDir) {
     // clean up files created by node/ubuntu user that cannot be deleted by jenkins. Note: uses global environment variable
-    ctx.sh("[ -d \"$ctx.WORKSPACE/test-output\" ] && docker run --rm -u node --mount type=bind,source='$ctx.WORKSPACE',target=/$containerWorkDir $containerImage chown 1000: -R $containerWorkDir")    
+    ctx.sh("[ -d \"$ctx.WORKSPACE/test-output\" ] && docker run --rm -u node --mount type=bind,source='$ctx.WORKSPACE',target=/$containerWorkDir $containerImage chown 1000: -R $containerWorkDir/test-output")
+    ctx.sh("[ -d \"$ctx.WORKSPACE/test-output\" ] && docker run --rm -u node --mount type=bind,source='$ctx.WORKSPACE',target=/$containerWorkDir $containerImage chown 1000: -R $containerWorkDir/test") 
   }
 
   static def analyseNodeJsCode(ctx, sonarQubeEnv, sonarScanner, params) {
