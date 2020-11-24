e41e49d8087feaec254467c83a842d12c6952db0
John Watson <johnwatson484@hotmail.com>
2020-11-24 15:21:23 +0000

    Change ownership of files

--- src/uk/gov/defra/ffc/Tests.groovy
+++ src/uk/gov/defra/ffc/Tests.groovy
@@ -77,8 +77,7 @@ class Tests implements Serializable {
 
   static def deleteOutput(ctx, containerImage, containerWorkDir) {
     // clean up files created by node/ubuntu user that cannot be deleted by jenkins. Note: uses global environment variable
-    //ctx.sh("[ -d \"$ctx.WORKSPACE/test-output\" ] && docker run --rm -u node --mount type=bind,source='$ctx.WORKSPACE/test-output',target=/$containerWorkDir/test-output $containerImage rm -rf test-output/*")
-    ctx.sh("chmod -R a+w \$PWD")
+    ctx.sh("[ -d \"$ctx.WORKSPACE/test-output\" ] && docker run --rm -u node --mount type=bind,source='$ctx.WORKSPACE/test-output',target=/$containerWorkDir/test-output $containerImage chown 1000: -R $ctx.WORKSPACE")    
   }
 
   static def analyseNodeJsCode(ctx, sonarQubeEnv, sonarScanner, params) {
