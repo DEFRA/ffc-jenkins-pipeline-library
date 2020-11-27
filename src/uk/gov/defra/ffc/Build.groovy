1a2a46e2a8f59d344a6e1ae2e7f8013b3f307f7b
Fareed Mohammed <fareed.mohammed.uk@gmail.com>
2020-11-27 00:19:44 +0000

    Snyk test running using a container image (#208)
    * initial: PR creation
    https://eaflood.atlassian.net/browse/PSD-1011
    
    * feat: changes for snyk
    
    * fix: updated version
    
    * fix: removed trailing space

--- src/uk/gov/defra/ffc/Build.groovy
+++ src/uk/gov/defra/ffc/Build.groovy
@@ -90,8 +90,17 @@ class Build implements Serializable {
     failOnIssues = shouldFailOnIssues(failOnIssues, pr)
     organisation = organisation ?: ctx.SNYK_ORG
     severity = severity ?: 'medium'
-    ctx.gitStatusWrapper(credentialsId: 'github-token', sha: Utils.getCommitSha(ctx), repo: Utils.getRepoName(ctx), gitHubContext: GitHubStatus.SnykTest.Context, description: GitHubStatus.SnykTest.Description) {
-      ctx.snykSecurity(snykInstallation: 'snyk-default', snykTokenId: 'snyk-token', failOnIssues: failOnIssues, organisation: organisation, severity: severity, targetFile: targetFile, additionalArguments: '--insecure')
-    }
+    
+    ctx.sh('mkdir -p -m 777 snyk-cli')
+    ctx.dir('snyk-cli') {
+      ctx.withCredentials([ctx.string(credentialsId: 'ffc-snyk-token', variable: 'snykToken')
+      ]) {
+          ctx.echo("SNYK TOKEN: $ctx.snykToken")
+          def script = "docker run -e 'SNYK_TOKEN=$ctx.snykToken' -e 'MONITOR=true' -v '$ctx.WORKSPACE:/project' snyk/snyk-cli:npm test --json > snyk-result.json --org=$organisation --severity-threshold=$severity --file=$targetFile"
+          ctx.gitStatusWrapper(credentialsId: 'github-token', sha: Utils.getCommitSha(ctx), repo: Utils.getRepoName(ctx), gitHubContext: GitHubStatus.SnykTest.Context, description: GitHubStatus.SnykTest.Description) {
+            ctx.sh(returnStatus: !failOnIssues, script: script)
+          }
+        }
+      }
   }
 }
