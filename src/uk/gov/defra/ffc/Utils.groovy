dfacf8e9d89eb8c6340171146bcdee745459c680
fareedmohammed <fareed.mohammed.uk@gmail.com>
2020-12-01 17:02:21 +0000

    feat:changes for commit message testing

--- src/uk/gov/defra/ffc/Utils.groovy
+++ src/uk/gov/defra/ffc/Utils.groovy
@@ -13,7 +13,7 @@ class Utils implements Serializable {
 
     commitMessage = escapeSpecialChars(commitMessage)
 
-    ctx.echo('output commit messge: /' + commitMessage.replaceAll("\\r\\n|\\r|\\n", "<br />") + '/')
+    ctx.echo('output commit messge: /' + commitMessage.replaceAll("\\r\\n|\\r|\\n", "<br />").replaceAll(/'/, /\'/) + '/')
 
     return commitMessage.replaceAll("\\r\\n|\\r|\\n", "<br />").replaceAll(/'/, /\'/)
   }
