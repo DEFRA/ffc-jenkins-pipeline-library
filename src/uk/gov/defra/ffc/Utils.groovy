06ac89bae6d46bbc462641a9cebb9f9c595a6ad9
fareedmohammed <fareed.mohammed.uk@gmail.com>
2020-12-01 11:00:00 +0000

    feat:changes for commit message testing

--- src/uk/gov/defra/ffc/Utils.groovy
+++ src/uk/gov/defra/ffc/Utils.groovy
@@ -77,7 +77,7 @@ class Utils implements Serializable {
   }
 
   static def escapeSpecialChars(str) {
-    return str.replace('\\', '\\\\\\\\').replace(/,/, /\,/).replace(/"/, /\"/).replace(/`/, /\`/)
+    return str.replace('\\', '\\\\\\\\').replace(/,/, /\,/).replace(/"/, /\"/).replace(/`/, /\`/).replace("'", "\\'")
   }
 
   /**
