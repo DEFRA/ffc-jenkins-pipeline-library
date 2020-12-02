3f0a85bc384251f8afabfd6b27f14be67fe051c1
fareedmohammed <fareed.mohammed.uk@gmail.com>
2020-12-02 13:08:25 +0000

    feat: create new method to handle commit message for GitHub releases

--- src/uk/gov/defra/ffc/Utils.groovy
+++ src/uk/gov/defra/ffc/Utils.groovy
@@ -82,7 +82,7 @@ class Utils implements Serializable {
   }
 
   static def formatCommitMessage(str) {
-    return str.replace("'", /'"'"'/).replaceAll("\\r\\n|\\r|\\n", "<br />")
+    return str.replaceAll("'", /'"'"'/).replaceAll("\\r\\n|\\r|\\n", "<br />")
   }
 
   /**
