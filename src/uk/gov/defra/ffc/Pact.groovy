e486c65d2d597ba2b66dc290ae4a5087d3139c64
fareedmohammed <fareed.mohammed.uk@gmail.com>
2020-12-03 16:05:54 +0000

    feat: changed to use docker container

--- src/uk/gov/defra/ffc/Pact.groovy
+++ src/uk/gov/defra/ffc/Pact.groovy
@@ -15,7 +15,7 @@ class Pact implements Serializable {
             def provider = pact.name.substring("$repoName-".length(), pact.name.indexOf(".json"))
             ctx.echo "Publishing ${pact.name} to broker"
            
-            def script = "docker --rm -e ${ctx.PACT_BROKER_URL}/pacts/provider/$provider/consumer/$repoName/version/$version+$commitSha -e ${ctx.pactUsername} -e ${ctx.pactPassword} pactfoundation/pact-cli:latest broker create-version-tag --pacticipant ${pact.name} --version ${version}+${commitSha} --tag main"                                  
+            def script = "docker --rm -e $ctx.PACT_BROKER_URL/pacts/provider/$provider/consumer/$repoName/version/$version+$commitSha -e $ctx.pactUsername -e $ctx.pactPassword pactfoundation/pact-cli:latest broker create-version-tag --pacticipant $pact.name --version $version+$commitSha --tag main"                                  
               ctx.gitStatusWrapper(credentialsId: 'github-token', sha: Utils.getCommitSha(ctx), repo: Utils.getRepoName(ctx), gitHubContext: GitHubStatus.PactBroker.Context, description: GitHubStatus.PactBroker.Description) {
               ctx.sh(returnStatus: !failOnIssues, script: script)
             }
