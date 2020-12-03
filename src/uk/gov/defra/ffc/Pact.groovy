68dfda123061055333527816cf7be44e2bedcf6e
fareedmohammed <fareed.mohammed.uk@gmail.com>
2020-12-03 15:17:07 +0000

    feat: changed to use docker container

--- src/uk/gov/defra/ffc/Pact.groovy
+++ src/uk/gov/defra/ffc/Pact.groovy
@@ -2,7 +2,28 @@ package uk.gov.defra.ffc
 
 class Pact implements Serializable {
   static def publishContractsToPactBroker(ctx, repoName, version, commitSha, branchName) {
-    ctx.withCredentials([
+
+    ctx.sh('mkdir -p -m 777 test-output')
+    ctx.dir('test-output') {
+      ctx.withCredentials([ctx.string(credentialsId: 'pact-broker-credentials', usernameVariable: 'pactUsername', passwordVariable: 'pactPassword')
+      ]) {
+
+          ctx.echo "Publish pacts to broker"
+          def pacts = ctx.findFiles glob: "$repoName-*.json"
+          ctx.echo "Found ${pacts.size()} pact file(s) to publish"
+          for (pact in pacts) {
+            def provider = pact.name.substring("$repoName-".length(), pact.name.indexOf(".json"))
+            ctx.echo "Publishing ${pact.name} to broker"
+           
+            def script = "docker --rm -e ${ctx.PACT_BROKER_URL}/pacts/provider/$provider/consumer/$repoName/version/$version+$commitSha -e ${ctx.pactUsername} -e ${ctx.pactPassword} pactfoundation/pact-cli:latest broker create-version-tag --pacticipant ${pact.name} --version ${version}+${commitSha} --tag main"                                  
+              ctx.gitStatusWrapper(credentialsId: 'github-token', sha: Utils.getCommitSha(ctx), repo: Utils.getRepoName(ctx), gitHubContext: GitHubStatus.PactBroker.Context, description: GitHubStatus.PactBroker.Description) {
+              ctx.sh(returnStatus: !failOnIssues, script: script)
+            }
+          }
+        }
+      }
+
+    /* ctx.withCredentials([
       ctx.usernamePassword(credentialsId: 'pact-broker-credentials', usernameVariable: 'pactUsername', passwordVariable: 'pactPassword')
     ]) {
       ctx.dir('test-output') {
@@ -15,6 +36,6 @@ class Pact implements Serializable {
           ctx.sh "curl -k -v -XPUT -H \"Content-Type: application/json\" --user ${ctx.pactUsername}:${ctx.pactPassword} -d@${pact.name} ${ctx.PACT_BROKER_URL}/pacts/provider/$provider/consumer/$repoName/version/$version+$commitSha/tags/$branchName"
         }
       }
-    }
+    } */
   }
 }
