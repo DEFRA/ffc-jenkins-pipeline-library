9de0120891143ebaa5b1b50975847f13b34ee25f
fareedmohammed <fareed.mohammed.uk@gmail.com>
2020-12-07 12:59:19 +0000

    feat: changed to use docker container

--- src/uk/gov/defra/ffc/Pact.groovy
+++ src/uk/gov/defra/ffc/Pact.groovy
@@ -16,28 +16,12 @@ class Pact implements Serializable {
             def provider = pact.name.substring("$repoName-".length(), pact.name.indexOf(".json"))
             ctx.echo "Publishing ${pact.name} to broker"
            
-            //def script = "docker run --rm -e PACT_DISABLE_SSL_VERIFICATION=true -e PACT_BROKER_BASE_URL=$ctx.PACT_BROKER_URL/pacts/provider/$provider/consumer/$repoName/version/$version+$commitSha -e PACT_BROKER_USERNAME=$ctx.pactUsername -e PACT_BROKER_PASSWORD=$ctx.pactPassword pactfoundation/pact-cli:latest broker create-version-tag --pacticipant $pact.name --version $version+$commitSha --tag main"
-            def script = "docker run --rm -w \$(pwd) -v \$(pwd):\$(pwd) -v /etc/ssl/certs/:/etc/ssl/certs/ -v /usr/local/share/ca-certificates/ -e PACT_BROKER_BASE_URL=$ctx.PACT_BROKER_URL -e PACT_BROKER_USERNAME=$ctx.pactUsername -e PACT_BROKER_PASSWORD=$ctx.pactPassword pactfoundation/pact-cli:latest broker publish --consumer-app-version $version+$commitSha $pact --tag main"
+            def script = "docker run --rm -w \$(pwd) -v \$(pwd):\$(pwd) -e PACT_DISABLE_SSL_VERIFICATION=true -e PACT_BROKER_BASE_URL=$ctx.PACT_BROKER_URL -e PACT_BROKER_USERNAME=$ctx.pactUsername -e PACT_BROKER_PASSWORD=$ctx.pactPassword pactfoundation/pact-cli:latest broker publish --consumer-app-version $version+$commitSha $pact --tag main"
               ctx.gitStatusWrapper(credentialsId: 'github-token', sha: Utils.getCommitSha(ctx), repo: Utils.getRepoName(ctx), gitHubContext: GitHubStatus.PactBrokerTest.Context, description: GitHubStatus.PactBrokerTest.Description) {
               ctx.sh(returnStatus: true, script: script)
             }
           }
         }
       }
-
-    /* ctx.withCredentials([
-      ctx.usernamePassword(credentialsId: 'pact-broker-credentials', usernameVariable: 'pactUsername', passwordVariable: 'pactPassword')
-    ]) {
-      ctx.dir('test-output') {
-        ctx.echo "Publish pacts to broker"
-        def pacts = ctx.findFiles glob: "$repoName-*.json"
-        ctx.echo "Found ${pacts.size()} pact file(s) to publish"
-        for (pact in pacts) {
-          def provider = pact.name.substring("$repoName-".length(), pact.name.indexOf(".json"))
-          ctx.echo "Publishing ${pact.name} to broker"
-          ctx.sh "curl -k -v -XPUT -H \"Content-Type: application/json\" --user ${ctx.pactUsername}:${ctx.pactPassword} -d@${pact.name} ${ctx.PACT_BROKER_URL}/pacts/provider/$provider/consumer/$repoName/version/$version+$commitSha/tags/$branchName"
-        }
-      }
-    } */
   }
 }
