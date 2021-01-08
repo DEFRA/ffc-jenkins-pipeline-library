7552d65ebc8a165dd3387a2340c8328029dde5c3
FayToward <43171822+FayToward@users.noreply.github.com>
2021-01-08 09:35:28 +0000

    Increase name limit (#222)
    Bug:
    - 'Build' removed from topic name and replaced with 'b' to help with limit of 50 characters

--- src/uk/gov/defra/ffc/Provision.groovy
+++ src/uk/gov/defra/ffc/Provision.groovy
@@ -193,7 +193,7 @@ class Provision implements Serializable {
   }
 
   private static def getBuildQueuePrefix (ctx, repoName, pr) {
-    return "$repoName-build$ctx.BUILD_NUMBER-$pr-"
+    return "$repoName-b$ctx.BUILD_NUMBER-$pr-"
   }
 
   private static def createPrDatabase(ctx, environment, repoName, pr) {
