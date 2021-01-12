9d25e8cde22c6c863f6909fd41b5d7a1f939994d
fareedmohammed <fareed.mohammed.uk@gmail.com>
2021-01-12 00:58:54 +0000

    fix: syntax error

--- src/uk/gov/defra/ffc/Utils.groovy
+++ src/uk/gov/defra/ffc/Utils.groovy
@@ -129,7 +129,7 @@ class Utils implements Serializable {
 
   static def sendNotification(channel, msg, color){
   
-    ctx.withCredentials([ctx.string(credentialsId: (channel == '#mainbuildfailures' ? 'slack-mainbuildfailures-channel-webhook' : 'slack-generalbuildfailures-channel-webhook'), variable: 'webHook')
+    ctx.withCredentials([ctx.string(credentialsId: ($channel == '#mainbuildfailures' ? 'slack-mainbuildfailures-channel-webhook' : 'slack-generalbuildfailures-channel-webhook'), variable: 'webHook')
     ]) {
 
       def script = "docker run -e SLACK_WEBHOOK=$ctx.webHook -e SLACK_MESSAGE=$msg -e SLACK_COLOR=$color technosophos/slack-notify:latest"
