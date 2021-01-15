b573b3828ee8121d245a8e9b81898ecc43dec938
John Watson <johnwatson484@hotmail.com>
2021-01-15 17:00:13 +0000

    Remove silent

--- src/uk/gov/defra/ffc/Release.groovy
+++ src/uk/gov/defra/ffc/Release.groovy
@@ -55,7 +55,7 @@ class Release implements Serializable {
     ctx.echo("Triggering release $versionTag for $repoName")
     boolean result = false
 
-    result = ctx.sh(returnStdout: true, script: "curl -s -X POST -H 'Authorization: token $token' --data-urlencode '{ \"tag_name\" : \"$versionTag\", \"name\" : \"Release $versionTag\", \"body\" : \" $releaseDescription \" }' https://api.github.com/repos/DEFRA/$repoName/releases")    
+    result = ctx.sh(returnStdout: true, script: "curl -X POST -H 'Authorization: token $token' --data-urlencode '{ \"tag_name\" : \"$versionTag\", \"name\" : \"Release $versionTag\", \"body\" : \" $releaseDescription \" }' https://api.github.com/repos/DEFRA/$repoName/releases")    
 
     if (exists(ctx, versionTag, repoName, token)) {
       ctx.echo('Release Successful')
