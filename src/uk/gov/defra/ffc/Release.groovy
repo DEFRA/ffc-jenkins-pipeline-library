33b8c7fcb22fa7379f237b56eef245ec24875c2c
John Watson <johnwatson484@hotmail.com>
2021-01-18 16:48:55 +0000

    wrapped args

--- src/uk/gov/defra/ffc/Release.groovy
+++ src/uk/gov/defra/ffc/Release.groovy
@@ -56,7 +56,7 @@ class Release implements Serializable {
     ctx.echo("Triggering release $versionTag for $repoName")
     boolean result = false
 
-    def json = JsonOutput.toJson('{"tag_name":$versionTag, "name": "Release $versionTag, "body": $releaseDescription}')
+    def json = JsonOutput.toJson('{"tag_name":${versionTag}, "name": "Release ${versionTag}", "body": ${releaseDescription}}')
 
     result = ctx.sh(returnStdout: true, script: "curl -v -X POST -H 'Authorization: token $token' -d $json https://api.github.com/repos/DEFRA/$repoName/releases")    
 
