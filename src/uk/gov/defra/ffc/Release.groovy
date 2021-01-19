f92cf2c4325a552a3df3181efb9724af6c0bc471
John Watson <johnwatson484@hotmail.com>
2021-01-19 12:49:04 +0000

    SED

--- src/uk/gov/defra/ffc/Release.groovy
+++ src/uk/gov/defra/ffc/Release.groovy
@@ -57,8 +57,9 @@ class Release implements Serializable {
     boolean result = false
 
     String json = JsonOutput.toJson(["tag_name":versionTag, "name": "Release ${versionTag}", "body": "${releaseDescription}"]).toString()
-    ctx.echo(json)
-    def script = "curl -v -X POST -H 'Authorization: token $token' -H 'Content-Type: application/json' -d '${json}' https://api.github.com/repos/DEFRA/$repoName/releases"
+    sanitizedJson = ctx.sh(returnStdout: true, script: "echo $json | sed 's/[][`~!@#$%^&*()-_=+{}\\|;:\",<.>/?\'\"\'\"\']/\\&/g'")
+    ctx.echo(sanitizedJson)
+    def script = "curl -v -X POST -H 'Authorization: token $token' -H 'Content-Type: application/json' -d '${sanitizedJson}' https://api.github.com/repos/DEFRA/$repoName/releases"
     //def script = "curl -v -X POST -H 'Authorization: token $token' -H 'Content-Type: application/json' -d '{"tag_name":"pr130","name":"Release pr130","body":"Update package.json\n\n### Patch\r\n- this is purely a \"test\" to understand if the ^fix I have done; works!\r\n\r\n- if it doesn't then that is a 'shame'\r\n- if we are happy (i.e. the whole % of the team`), then we can celebrate* \\\\ TODO delete / @ this branch ~#\r\n\r\n### !\"\u00a3$%^&*()~#'/\\,.\n"}' https://api.github.com/repos/DEFRA/$repoName/releases"
     ctx.echo(script)
     result = ctx.sh(returnStdout: true, script: script)
