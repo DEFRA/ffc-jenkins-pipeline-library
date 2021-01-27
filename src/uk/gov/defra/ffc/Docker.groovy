7deff88e5db440a853d7a932482dbcb29578177b
fareed-mohammed <fareed.mohammed.uk@gmail.com>
2021-01-27 15:26:20 +0000

    feat:add npm registry variablepackage uk.gov.defra.ffc

--- src/uk/gov/defra/ffc/Docker.groovy
+++ src/uk/gov/defra/ffc/Docker.groovy
@@ -9,7 +9,7 @@ class Docker implements Serializable {
 
   static def buildAndPushContainerImage(ctx, credentialsId, registry, imageName, tag) {
     ctx.docker.withRegistry("https://$registry", credentialsId) {
-      ctx.sh('docker-compose -f docker-compose.yaml build')
+      ctx.sh("docker-compose -f docker-compose.yaml build --build-arg NPM_REGISTRY=${ctx.NPM_REGISTRY}")
       ctx.sh("docker tag $imageName $registry/$imageName:$tag")
       ctx.sh("docker push $registry/$imageName:$tag")
     }
