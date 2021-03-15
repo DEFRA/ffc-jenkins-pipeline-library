9f5ac724c50dc3ffcd27610fa96a681caa03e61e
fareed-mohammed <fareed.mohammed.uk@gmail.com>
2021-03-15 18:05:13 +0000

    feat:updated for changes to add console logs~

--- src/uk/gov/defra/ffc/ConsoleLogs.groovy
+++ src/uk/gov/defra/ffc/ConsoleLogs.groovy
@@ -13,6 +13,6 @@ class ConsoleLogs implements Serializable {
 
     ctx.echo("Date: $logName")
     ctx.echo("log name: log_${logName}.txt")
-    ctx.sh(script: $url > "/var/log/jenkins/console/$logName", returnStdout: true)
+    ctx.sh(script: "${url} > /var/log/jenkins/console/${logName}", returnStdout: true)
   }
 }
