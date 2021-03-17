2e5c7ccf827bf3755b01a551b747148cdadd2275
fareed-mohammed <fareed.mohammed.uk@gmail.com>
2021-03-17 14:29:54 +0000

    feat:updated for changes to add console logs~

--- src/uk/gov/defra/ffc/ConsoleLogs.groovy
+++ src/uk/gov/defra/ffc/ConsoleLogs.groovy
@@ -21,7 +21,7 @@ class ConsoleLogs implements Serializable {
     ctx.sh("ls")
     ctx.dir('../../../../../../../../../log/jenkins/console/') {
     //ctx.sh("cd /var/log/jenkins/console/")
-    ctx.sh("chmod 777 /var/log/jenkins/console/")
+    ctx.sh("chown -R $USER:$USER /var/log/jenkins/console/")
     ctx.sh("ls")
 
     // ctx.sh("chmod 777 /var/log/jenkins/console/")
