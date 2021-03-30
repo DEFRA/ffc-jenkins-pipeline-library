9e007126e8083da9c1edb68b7a5342dd7c9a743f
fareed-mohammed <fareed.mohammed.uk@gmail.com>
2021-03-30 18:28:43 +0100

    feat:updated for changes to add console logs~

--- vars/consoleLogs.groovy
+++ vars/consoleLogs.groovy
@@ -1,9 +1,9 @@
 import uk.gov.defra.ffc.ConsoleLogs
 
-void save(String jenkinsUrl, String repoName, String branch, String buildNumber) {
+void save(String jenkinsUrl, String repoName, String branch, String buildNumber, String logFilePath) {
   ConsoleLogs.save(this, jenkinsUrl, repoName, branch, buildNumber, logFilePath)
 }
 
-void save(String jenkinsUrl, String repoName, String buildNumber) {
+void save(String jenkinsUrl, String repoName, String buildNumber, String logFilePath) {
   ConsoleLogs.save(this, jenkinsUrl, repoName, buildNumber, logFilePath)
 }
