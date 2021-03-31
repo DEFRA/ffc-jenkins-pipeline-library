440411c56cd5bbfd6514a74fec5b91888e7635ae
fareed-mohammed <fareed.mohammed.uk@gmail.com>
2021-03-31 13:13:31 +0100

    feat:updated for changes to add console logs~

--- src/uk/gov/defra/ffc/ConsoleLogs.groovy
+++ src/uk/gov/defra/ffc/ConsoleLogs.groovy
@@ -18,11 +18,11 @@ class ConsoleLogs implements Serializable {
   }
 
   static def saveLogFile(ctx, url, logFilePath) {
-    def logFileName = new Date().format("yyyy-MM-dd_HH:mm:ss", TimeZone.getTimeZone('UTC'))
+    def logFileDateTime = new Date().format("yyyy-MM-dd_HH:mm:ss", TimeZone.getTimeZone('UTC'))
     
     ctx.sh("[ -d $logFilePath ]  && docker run --rm -u root --privileged --mount type=bind,source=$logFilePath,target=/home/node defradigital/node-development chown $ctx.JENKINS_USER_ID:$ctx.JENKINS_GROUP_ID -R -v .")
    
-    def script = "curl $url > $logFilePath/log_${logFileName}.txt"
+    def script = "curl $url > $logFilePath/log_${logFileDateTime}.txt"
     ctx.echo("script: $script")
     ctx.sh(script: script, returnStdout: true)
   }
