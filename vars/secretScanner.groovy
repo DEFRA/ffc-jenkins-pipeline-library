38def43a27199a1ef32daa0ff6fc5b7cf73ecbf2
John Watson <johnwatson484@hotmail.com>
2021-04-09 14:28:32 +0100

    Fix secret scanning signature

--- vars/secretScanner.groovy
+++ vars/secretScanner.groovy
@@ -1,6 +1,6 @@
 import uk.gov.defra.ffc.SecretScanner
 
-Boolean scanWithinWindow(String githubCredentialId, String dockerImgName, String githubOwner, String repositoryPrefix, Date() scanWindowHrs, String[] excludeStrings, String slackChannel="") {
+Boolean scanWithinWindow(String githubCredentialId, String dockerImgName, String githubOwner, String repositoryPrefix, Date scanWindowHrs, String[] excludeStrings, String slackChannel="") {
   return SecretScanner.scanWithinWindow(this, githubCredentialId, dockerImgName, githubOwner, repositoryPrefix, scanWindowHrs, excludeStrings, slackChannel)
 }
 
