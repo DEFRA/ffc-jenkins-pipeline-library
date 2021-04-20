824812011ab951f4e637cdf500f9716a35e7f216
steve-dickinson <steve.dickinson@defra.gov.uk>
2021-04-20 15:01:55 +0100

    withEnv changes

--- src/uk/gov/defra/ffc/Tests.groovy
+++ src/uk/gov/defra/ffc/Tests.groovy
@@ -164,15 +164,9 @@ class Tests implements Serializable {
             def domain = serverConfig['ingress.server'].trim()
             def hostname = pr == '' ? endpoint : "${endpoint}-pr${pr}"
 
-            ctx.withEnv(["BROWSERSTACK_USERNAME=$ctx.browserStackUsername"]) {
-              echo "BROWSERSTACK_USERNAME = ${ctx.BROWSERSTACK_USERNAME}"
-            }
-
-            ctx.withEnv(["BROWSERSTACK_ACCESS_TOKEN=$ctx.browserStackAccessToken"]) {
-              echo "BROWSERSTACK_ACCESS_TOKEN = ${ctx.BROWSERSTACK_ACCESS_TOKEN}"
-            }
-
-            ctx.withEnv(["TEST_ENVIRONMENT_ROOT_URL=https://${hostname}.${domain}"]) {
+            ctx.withEnv(["TEST_ENVIRONMENT_ROOT_URL=https://${hostname}.${domain}", 
+                          "BROWSERSTACK_USERNAME=${ctx.browserStackUsername}", 
+                          "BROWSERSTACK_ACCESS_TOKEN=${ctx.browserStackAccessToken}"]) {
             ctx.sh('docker-compose -f docker-compose.yaml build')
             ctx.sh('docker-compose run wdio-cucumber')
             }
