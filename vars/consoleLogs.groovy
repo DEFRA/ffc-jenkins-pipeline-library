5e39327ae0b5c5d3ac2584fd0d99e85485a257a9
fareed-mohammed <fareed.mohammed.uk@gmail.com>
2021-04-26 16:33:47 +0100

    refactor: used jenkins build url for the path

--- vars/consoleLogs.groovy
+++ vars/consoleLogs.groovy
@@ -3,7 +3,3 @@ import uk.gov.defra.ffc.ConsoleLogs
 void save(String logFilePath) {
   ConsoleLogs.save(this, logFilePath)
 }
-
-void save(String logFilePath) {
-  ConsoleLogs.save(this, logFilePath)
-}
