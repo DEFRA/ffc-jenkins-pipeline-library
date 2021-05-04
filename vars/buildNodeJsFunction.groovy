aeb0847708097a2b209de60304c36bda4944fe8e
steve-dickinson <steve.dickinson@defra.gov.uk>
2021-05-04 07:42:44 +0100

    Add buildNodeJsFunction

---
+++ vars/buildNodeJsFunction.groovy
@@ -0,0 +1,163 @@
+void call(Map config=[:]) {
+  String defaultBranch = 'main'
+  String environment = 'snd'
+  String containerSrcFolder = '\\/home\\/node'
+  String nodeDevelopmentImage = 'defradigital/node-development'
+  String localSrcFolder = '.'
+  String lcovFile = './test-output/lcov.info'
+  String repoName = ''
+  String pr = ''
+  String tag = ''
+  String mergedPrNo = ''
+
+  node {
+    try {
+
+      stage('Ensure clean workspace') {
+        deleteDir()
+      }
+
+      stage('Set default branch') {
+        defaultBranch = build.getDefaultBranch(defaultBranch, config.defaultBranch)
+      }
+
+      stage('Set environment') {
+        environment = config.environment != null ? config.environment : environment
+      }
+
+      stage('Checkout source code') {
+        build.checkoutSourceCode(defaultBranch)
+      }
+
+      stage('Set PR and tag variables') {
+        def version = version.getPackageJsonVersion()
+        (repoName, pr, tag, mergedPrNo) = build.getVariables(version, defaultBranch)
+      }
+
+      if (pr != '') {
+        stage('Verify version incremented') {
+          version.verifyPackageJsonIncremented(defaultBranch)
+        }
+      }
+
+      if (config.containsKey('validateClosure')) {
+        config['validateClosure']()
+      }
+
+      stage('npm audit') {
+        build.npmAudit(config.npmAuditLevel, config.npmAuditLogType, config.npmAuditFailOnIssues, nodeDevelopmentImage, containerSrcFolder, pr)
+      }
+
+      stage('Snyk test') {
+        build.snykTest(config.snykFailOnIssues, config.snykOrganisation, config.snykSeverity, pr)
+      }
+
+      stage('Provision any required resources') {
+        provision.createResources(environment, repoName, pr)
+      }
+
+      if (config.containsKey('buildClosure')) {
+        config['buildClosure']()
+      }
+
+      if (fileExists('./docker-compose.test.yaml')) {
+        stage('Build test image') {
+          build.buildTestImage(DOCKER_REGISTRY_CREDENTIALS_ID, DOCKER_REGISTRY, repoName, BUILD_NUMBER, tag)
+        }
+
+        stage('Run tests') {
+          build.runTests(repoName, repoName, BUILD_NUMBER, tag, pr, environment)
+        }
+
+        stage('Create JUnit report') {
+          test.createJUnitReport()
+        }
+
+        stage('Fix lcov report') {
+          utils.replaceInFile(containerSrcFolder, localSrcFolder, lcovFile)
+        }
+
+        if (pr == '') {
+          stage('Publish pact broker') {
+            pact.publishContractsToPactBroker(repoName, version.getPackageJsonVersion(), utils.getCommitSha())
+          }
+        }
+      }
+
+      stage('SonarCloud analysis') {
+        test.analyseNodeJsCode(SONARCLOUD_ENV, SONAR_SCANNER, repoName, BRANCH_NAME, defaultBranch, pr)
+      }
+
+      if (fileExists('./docker-compose.zap.yaml')) {
+        stage('Run Zap Scan') {
+          test.runZapScan(repoName, BUILD_NUMBER, tag)
+        }
+      }
+
+      if (fileExists('./docker-compose.pa11y.yaml')) {
+        stage('Run Accessibility tests') {
+          test.runPa11y(repoName, BUILD_NUMBER, tag)
+        }
+      }
+
+      if (config.containsKey('testClosure')) {
+        config['testClosure']()
+      }
+
+      stage('Build & push container image') {
+        build.buildAndPushContainerImage(DOCKER_REGISTRY_CREDENTIALS_ID, DOCKER_REGISTRY, repoName, tag)
+      }
+
+      if (pr == '') {
+        stage('Trigger GitHub release') {
+          withCredentials([
+            string(credentialsId: 'github-auth-token', variable: 'gitToken')
+          ]) {
+            String commitMessage = utils.getCommitMessage()
+            release.trigger(tag, repoName, commitMessage, gitToken)
+          }
+        }
+      }
+
+      if (config.containsKey('deployClosure')) {
+        config['deployClosure']()
+      }
+
+      if (fileExists('./test/acceptance/docker-compose.yaml') && hasHelmChart) {
+        stage('Run Acceptance Tests') {
+          test.runAcceptanceTests(pr, environment, repoName)
+        }
+      }
+
+    } catch(e) {
+      def errMsg = utils.getErrorMessage(e)
+      echo("Build failed with message: $errMsg")
+
+      stage('Send build failure slack notification') {
+        notifySlack.buildFailure('generalbuildfailures', defaultBranch)
+      }
+
+      if (config.containsKey('failureClosure')) {
+        config['failureClosure']()
+      }
+
+      throw e
+    } finally {
+      stage('Change ownership of outputs') {
+        test.changeOwnershipOfWorkspace(nodeDevelopmentImage, containerSrcFolder)
+      }      
+ 
+      stage('Clean up resources') {
+        provision.deleteBuildResources(repoName, pr)
+      }
+
+      if (config.containsKey('finallyClosure')) {
+        config['finallyClosure']()
+      }
+
+      stage('Publish to Log Analytics') {
+        consoleLogs.save('/var/log/jenkins/console')
+      }
+    }
+  }
+}
---
+++ vars/buildNodeJsFunction.md
@@ -0,0 +1,71 @@
+# buildNodeJs
+
+> The default build configuration for a Azure Function Node.js project.
+
+In order to use it with the default conventions simply add the following to
+your Jenkinsfile.
+For the details of what happens please review the
+[buildNodeJsFunction](buildNodeJsFunction.groovy) script.
+
+```
+@Library('defra-library@v-9') _
+
+buildNodeJsFunction()
+```
+
+## npm Audit
+
+By default an [npm audit](https://docs.npmjs.com/cli/audit) will run for every
+build. If any issues are identified at a level of `moderate` or above the build
+will be marked as failed for PR builds only. When the step runs during a build
+of the main branch it will warn only.
+
+The job has been setup to allow several options to be configured, details of
+those options are available in [build](build.md). In order to override the
+options when running the `buildNodeJsFunction` pipeline, the config option requires the
+keys `npmAuditLevel`, `npmAuditLogType` and `npmAuditFailOnIssues`.
+An example overriding the default values:
+
+```
+buildNodeJsFunction npmAuditLevel: 'low', npmAuditLogType: 'json', npmAuditFailOnIssues: true
+```
+
+## Synk
+
+By default a
+[snyk test](https://support.snyk.io/hc/en-us/articles/360003812578#UUID-c88e66cf-431c-9ab1-d388-a8f82991c6e0)
+will run during the build. This is achieved through the use of the
+[snyk security scanner](https://plugins.jenkins.io/snyk-security-scanner/)
+plugin for Jenkins.
+The default settings will fail the build when an issue of `medium` or above is
+identified on PR builds only. When the step runs during a build of the main
+branch it will warn only.
+The job has been setup to allow several options to be configured, details of
+those options are available in [build](build.md). In order to override the
+options from the `buildNodeJs` pipeline the config object requires the keys
+`snykFailOnIssues`, `snykOrganisation` and `snykSeverity`.
+An example overriding the default values:
+
+```
+buildNodeJsFunction environment: 'dev', snykFailOnIssues: true, snykOrganisation: 'my-org-name', snykSeverity: 'high'
+```
+
+## Default branch
+The build will assume the default branch in the repository is named `main`.  If not the default can be supplied in the build configuration.
+
+For example:
+
+```
+buildNodeJsFunction defaultBranch: 'master'
+```
+
+## Environment
+
+As part of the PR workflow, every commit will be dynamically deployed to the default Sandpit cluster, `snd`.  This also applied to all main branch code post merge.  
+If a different environment within the Sandpit environment should be used for this purpose then the default can be overriden.
+
+For example:
+
+```
+buildNodeJsFunction project: 'FFCDemoDotNetCoreProjectName', environment: 'snd2'
+```
