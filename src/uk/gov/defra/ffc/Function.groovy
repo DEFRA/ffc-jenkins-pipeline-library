c0395ee4dec03f6c441bd9fb060308802b75c5bd
steve-dickinson <steve.dickinson@defra.gov.uk>
2021-05-10 12:21:55 +0100

    replace hypen in storage name

--- src/uk/gov/defra/ffc/Function.groovy
+++ src/uk/gov/defra/ffc/Function.groovy
@@ -8,7 +8,7 @@ class Function implements Serializable {
   }
 
   static def createFunction(ctx, repoName){
-    def azCreateFunction = "az functionapp create -n $repoName --storage-account $repoName --consumption-plan-location ${ctx.AZURE_REGION} --app-insights ${ctx.AZURE_APPLICATION_INSIGHTS} --runtime node -g ${ctx.AZURE_FUNCTION_RESOURCE_GROUP} --functions-version 3"
+    def azCreateFunction = "az functionapp create -n $repoName --storage-account ${repoName.replace('-','')} --consumption-plan-location ${ctx.AZURE_REGION} --app-insights ${ctx.AZURE_APPLICATION_INSIGHTS} --runtime node -g ${ctx.AZURE_FUNCTION_RESOURCE_GROUP} --functions-version 3"
     ctx.sh("$azCreateFunction")
   }
 
