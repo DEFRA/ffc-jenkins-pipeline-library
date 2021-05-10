ed6d235849e6ea24afc191e46a48bba34cc7f9b3
steve-dickinson <steve.dickinson@defra.gov.uk>
2021-05-10 12:26:52 +0100

    replace hypen in storage name

--- src/uk/gov/defra/ffc/Function.groovy
+++ src/uk/gov/defra/ffc/Function.groovy
@@ -8,12 +8,13 @@ class Function implements Serializable {
   }
 
   static def createFunction(ctx, repoName){
-    def azCreateFunction = "az functionapp create -n $repoName --storage-account ${repoName.replace('-','')} --consumption-plan-location ${ctx.AZURE_REGION} --app-insights ${ctx.AZURE_APPLICATION_INSIGHTS} --runtime node -g ${ctx.AZURE_FUNCTION_RESOURCE_GROUP} --functions-version 3"
+    def azCreateFunction = "az functionapp create -n $repoName --storage-account $repoName --consumption-plan-location ${ctx.AZURE_REGION} --app-insights ${ctx.AZURE_APPLICATION_INSIGHTS} --runtime node -g ${ctx.AZURE_FUNCTION_RESOURCE_GROUP} --functions-version 3"
     ctx.sh("$azCreateFunction")
   }
 
   static def createFunctionStorage(ctx, repoName){
-    def azCreateFunctionStorage = "az storage account create -n $repoName -l ${ctx.AZURE_REGION} -g ${ctx.AZURE_FUNCTION_RESOURCE_GROUP} --sku Standard_LRS"
+    def storageAccountName = repoName.replace('-','')
+    def azCreateFunctionStorage = "az storage account create -n $storageAccountName -l ${ctx.AZURE_REGION} -g ${ctx.AZURE_FUNCTION_RESOURCE_GROUP} --sku Standard_LRS"
     ctx.sh("$azCreateFunctionStorage")
   }
 }
