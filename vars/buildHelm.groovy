3f88849b0e0d5f4d098fc02cadadea3f6647069f
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2021-06-14 10:20:14 +0100

    Fix Helm only pipeline to set correct deployment pipeline name (#251)
    * Fix Helm only pipeline to set correct deployment pipeline name
    * Update build helm readme

--- vars/buildHelm.groovy
+++ vars/buildHelm.groovy
@@ -4,6 +4,8 @@ void call(Map config=[:]) {
   String tag = ''
   String defaultBranch = 'main'
   String environment = 'snd'
+  Boolean triggerDeployment = config.triggerDeployment != null ? config.triggerDeployment : true
+  String deploymentPipelineName = ''
 
   node {
     try {
@@ -31,45 +33,50 @@ void call(Map config=[:]) {
       }
 
       if (pr != '') {
-          stage('Helm install') {
-            helm.deployChart(environment, DOCKER_REGISTRY, repoName, tag, pr)
-          }
-        } else {
-          stage('Publish chart') {
-            helm.publishChart(DOCKER_REGISTRY, repoName, tag, HELM_CHART_REPO_TYPE)
-          }
+        stage('Helm install') {
+          helm.deployChart(environment, DOCKER_REGISTRY, repoName, tag, pr)
+        }
+      } else {
+        stage('Publish chart') {
+          helm.publishChart(DOCKER_REGISTRY, repoName, tag, HELM_CHART_REPO_TYPE)
+        }
 
-          stage('Trigger GitHub release') {
-            withCredentials([
-              string(credentialsId: 'github-auth-token', variable: 'gitToken')
-            ]) {
-              String commitMessage = utils.getCommitMessage()
-              release.trigger(tag, repoName, commitMessage, gitToken)
-            }
+        stage('Trigger GitHub release') {
+          withCredentials([
+            string(credentialsId: 'github-auth-token', variable: 'gitToken')
+          ]) {
+            String commitMessage = utils.getCommitMessage()
+            release.trigger(tag, repoName, commitMessage, gitToken)
+          }
+        }
+
+        if (triggerDeployment) {
+          stage('Set deployment pipeline name') {
+            deploymentPipelineName = config.deploymentPipelineName != null ? config.deploymentPipelineName : "${repoName}-deploy"
           }
 
           stage('Trigger Deployment') {
-            if (utils.checkCredentialsExist("$repoName-deploy-token")) {            
+            if (utils.checkCredentialsExist("$repoName-deploy-token")) {
               withCredentials([
                 string(credentialsId: "$repoName-deploy-token", variable: 'jenkinsToken')
               ]) {
-                deploy.trigger(JENKINS_DEPLOY_SITE_ROOT, repoName, jenkinsToken, ['chartVersion': tag, 'environment': environment, 'helmChartRepoType': HELM_CHART_REPO_TYPE])
+                deploy.trigger(JENKINS_DEPLOY_SITE_ROOT, deploymentPipelineName, jenkinsToken, ['chartVersion': tag, 'environment': environment, 'helmChartRepoType': HELM_CHART_REPO_TYPE])
               }
-            } else {            
+            } else {
               withCredentials([
                 string(credentialsId: 'default-deploy-token', variable: 'jenkinsToken')
               ]) {
-                deploy.trigger(JENKINS_DEPLOY_SITE_ROOT, repoName, jenkinsToken, ['chartVersion': tag, 'environment': environment, 'helmChartRepoType': HELM_CHART_REPO_TYPE])
+                deploy.trigger(JENKINS_DEPLOY_SITE_ROOT, deploymentPipelineName, jenkinsToken, ['chartVersion': tag, 'environment': environment, 'helmChartRepoType': HELM_CHART_REPO_TYPE])
               }
             }
           }
         }
-
+      }
     } catch(e) {
       notifySlack.buildFailure(e.message, "#generalbuildfailures")
       throw e
     } finally {
-     
+
       stage('Publish to Log Analytics') {
         consoleLogs.save('/var/log/jenkins/console')
       }
