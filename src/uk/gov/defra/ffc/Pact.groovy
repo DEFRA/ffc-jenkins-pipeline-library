ec87f1795481651602fe91784c2e87f257723662
John Watson <johnwatson484@hotmail.com>
2021-09-28 13:06:29 +0100

    Assign

--- src/uk/gov/defra/ffc/Pact.groovy
+++ src/uk/gov/defra/ffc/Pact.groovy
@@ -15,8 +15,10 @@ class Pact implements Serializable {
         for (pact in pacts) {
           def provider = pact.name.substring("$repoName-".length(), pact.name.indexOf(".json"))
           ctx.echo "Publishing ${pact.name} to broker"
-          ctx.echo "u: ${ctx.pactUsername}"
-          ctx.echo "p: ${ctx.pactPassword}"
+          String u = ctx.pactUsername
+          String p = ctx.pactUserPassword
+          ctx.echo "u: ${u}"
+          ctx.echo "p: ${p}"
           
           def script = "docker run --rm -w \$(pwd) -v \$(pwd):\$(pwd) -e PACT_DISABLE_SSL_VERIFICATION=false -e PACT_BROKER_BASE_URL=$ctx.PACT_BROKER_URL -e PACT_BROKER_USERNAME=$ctx.pactUsername -e PACT_BROKER_PASSWORD=$ctx.pactPassword pactfoundation/pact-cli:latest broker publish --consumer-app-version $version+$commitSha $pact --tag main"
             ctx.gitStatusWrapper(credentialsId: 'github-token', sha: Utils.getCommitSha(ctx), repo: Utils.getRepoName(ctx), gitHubContext: GitHubStatus.PactBrokerPublish.Context, description: GitHubStatus.PactBrokerPublish.Description) {
