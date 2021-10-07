864c31903ea1e6fb3bbbbfe87e704629fe2f95a5
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-10-07 18:13:59 +0100

    test: trying some more stuff

--- src/uk/gov/defra/ffc/Pact.groovy
+++ src/uk/gov/defra/ffc/Pact.groovy
@@ -21,9 +21,14 @@ class Pact implements Serializable {
           ctx.sh "echo $ctx.pactPassword"
           def password = ctx.pactPassword
           ctx.echo password
+          if (password == ctx.pactPassword) {
+            ctx.echo "MATCH"
+          } else {
+            ctx.echo "DIFFER"
+          }
           ctx.echo "$ctx.PACT_BROKER_PASSWORD"
 
-          def script = "docker run --rm -w \$(pwd) -v \$(pwd):\$(pwd) -e PACT_DISABLE_SSL_VERIFICATION=false -e PACT_BROKER_BASE_URL=$ctx.PACT_BROKER_URL -e PACT_BROKER_USERNAME=$ctx.PACT_BROKER_USERNAME -e PACT_BROKER_PASSWORD=$ctx.pactPassword pactfoundation/pact-cli:latest broker publish --consumer-app-version $version+$commitSha $pact --tag main"
+          def script = "docker run --rm -w \$(pwd) -v \$(pwd):\$(pwd) -e PACT_DISABLE_SSL_VERIFICATION=false -e PACT_BROKER_BASE_URL=$ctx.PACT_BROKER_URL -e PACT_BROKER_USERNAME=$ctx.PACT_BROKER_USERNAME -e PACT_BROKER_PASSWORD=$password pactfoundation/pact-cli:latest broker publish --consumer-app-version $version+$commitSha $pact --tag main"
             ctx.gitStatusWrapper(credentialsId: 'github-token', sha: Utils.getCommitSha(ctx), repo: Utils.getRepoName(ctx), gitHubContext: GitHubStatus.PactBrokerPublish.Context, description: GitHubStatus.PactBrokerPublish.Description) {
             ctx.sh(returnStatus: true, script: script)
           }
