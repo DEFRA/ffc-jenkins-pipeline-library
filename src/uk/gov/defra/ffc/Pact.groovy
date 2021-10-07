cddbfe5c877c0fd7bb01f705f9d75a2fa3a167cb
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-10-07 17:24:10 +0100

    test: go back to credential password

--- src/uk/gov/defra/ffc/Pact.groovy
+++ src/uk/gov/defra/ffc/Pact.groovy
@@ -21,16 +21,7 @@ class Pact implements Serializable {
           ctx.echo "$ctx.pactPassword"
           ctx.echo "$ctx.PACT_BROKER_PASSWORD"
 
-          if (ctx.pactPassword == '') {
-            ctx.echo "ctx.pactPassword is empty..."
-          }
-          if (ctx.pactPassword == ctx.PACT_BROKER_PASSWORD) {
-            ctx.echo "Passwords are the same."
-          } else {
-            ctx.echo "Passwords are different!"
-          }
-
-          def script = "docker run --rm -w \$(pwd) -v \$(pwd):\$(pwd) -e PACT_DISABLE_SSL_VERIFICATION=false -e PACT_BROKER_BASE_URL=$ctx.PACT_BROKER_URL -e PACT_BROKER_USERNAME=$ctx.PACT_BROKER_USERNAME -e PACT_BROKER_PASSWORD=$ctx.PACT_BROKER_PASSWORD pactfoundation/pact-cli:latest broker publish --consumer-app-version $version+$commitSha $pact --tag main"
+          def script = "docker run --rm -w \$(pwd) -v \$(pwd):\$(pwd) -e PACT_DISABLE_SSL_VERIFICATION=false -e PACT_BROKER_BASE_URL=$ctx.PACT_BROKER_URL -e PACT_BROKER_USERNAME=$ctx.PACT_BROKER_USERNAME -e PACT_BROKER_PASSWORD=$ctx.pactPassword pactfoundation/pact-cli:latest broker publish --consumer-app-version $version+$commitSha $pact --tag main"
             ctx.gitStatusWrapper(credentialsId: 'github-token', sha: Utils.getCommitSha(ctx), repo: Utils.getRepoName(ctx), gitHubContext: GitHubStatus.PactBrokerPublish.Context, description: GitHubStatus.PactBrokerPublish.Description) {
             ctx.sh(returnStatus: true, script: script)
           }
