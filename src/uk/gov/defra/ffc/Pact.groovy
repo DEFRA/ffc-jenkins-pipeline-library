d8b0b51bc1c80da8c6de928aae2c33d5ff0b9ea8
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-10-08 12:43:09 +0100

    test: trying some more stuff

--- src/uk/gov/defra/ffc/Pact.groovy
+++ src/uk/gov/defra/ffc/Pact.groovy
@@ -19,21 +19,28 @@ class Pact implements Serializable {
           // Use PACT_BROKER_PASSWORD rather than pactPassword as it is being
           // mangled and fails authenication. To prevent password from being
           // logged, set +x to disable command logging.
-          def script = '''
+          // def script = '''
+          //   set +x \
+          //   docker run --rm -w \$(pwd) -v \$(pwd):\$(pwd) -e PACT_DISABLE_SSL_VERIFICATION=false \
+          //   -e PACT_BROKER_BASE_URL=\$PACT_BROKER_URL -e PACT_BROKER_USERNAME=$pactUsername \
+          //   -e PACT_BROKER_PASSWORD="\$PACT_BROKER_PASSWORD" pactfoundation/pact-cli:latest \
+          //   broker publish --consumer-app-version \
+          //   ''' + version + '''+''' + commitSha + ''' ''' + pact + ''' --tag main
+          //   '''
+          def script = """
             set +x \
             docker run --rm -w \$(pwd) -v \$(pwd):\$(pwd) -e PACT_DISABLE_SSL_VERIFICATION=false \
-            -e PACT_BROKER_BASE_URL=\$PACT_BROKER_URL -e PACT_BROKER_USERNAME=$pactUsername \
-            -e PACT_BROKER_PASSWORD="\$PACT_BROKER_PASSWORD" pactfoundation/pact-cli:latest \
-            broker publish --consumer-app-version \
-            ''' + version + '''+''' + commitSha + ''' ''' + pact + ''' --tag main
-            '''
+            -e PACT_BROKER_BASE_URL=$PACT_BROKER_URL -e PACT_BROKER_USERNAME=$ctx.pactUsername \
+            -e PACT_BROKER_PASSWORD="$PACT_BROKER_PASSWORD" pactfoundation/pact-cli:latest \
+            broker publish --consumer-app-version $version+$commitSha $pact --tag main
+            """
 
             ctx.gitStatusWrapper(credentialsId: 'github-token', sha: Utils.getCommitSha(ctx), repo: Utils.getRepoName(ctx), gitHubContext: GitHubStatus.PactBrokerPublish.Context, description: GitHubStatus.PactBrokerPublish.Description) {
-            def output = ctx.sh(returnStdout: true, script: script)
+            def output = ctx.sh(returnStatus: true, script: script)
             ctx.echo "output from command: $output"
 
             if (output == 1) {
-              ctx.error("Error occurred during publishing of pacts, pleaes check the log for further details.")
+              ctx.error("Error occurred during publishing of pacts, please check the log for further details.")
             } else {
               ctx.echo("Pacts published successfully.")
             }
