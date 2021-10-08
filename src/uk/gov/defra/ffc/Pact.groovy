5b45fd7ddfb05e9ebd0793012918e646154a5c2e
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-10-08 11:04:44 +0100

    test: trying some more stuff

--- src/uk/gov/defra/ffc/Pact.groovy
+++ src/uk/gov/defra/ffc/Pact.groovy
@@ -16,43 +16,21 @@ class Pact implements Serializable {
           def provider = pact.name.substring("$repoName-".length(), pact.name.indexOf(".json"))
           ctx.echo "Publishing ${pact.name} to broker"
 
-          ctx.echo ctx.pactPassword
-          ctx.echo ctx.PACT_BROKER_PASSWORD
-          ctx.sh "echo $ctx.pactPassword"
-          def password = ctx.pactPassword
-          ctx.echo password
-
-          def passwordSplit = password.split('L')
-          for ( String values : passwordSplit ) {
-            ctx.echo values
-          }
-
-          if (password == ctx.pactPassword) {
-            ctx.echo "MATCH"
-          } else {
-            ctx.echo "DIFFER"
-          }
-          ctx.echo "$ctx.PACT_BROKER_PASSWORD"
-          ctx.echo "pactuser01"
-          ctx.echo 'pactuser01'
-
           def script = """
-            echo $ctx.pactPassword \
-            pbpassword=\$(echo $ctx.pactPassword | base64 --decode) \
-            echo \$pbpassword \
+            set +x
             docker run --rm -w \$(pwd) -v \$(pwd):\$(pwd) -e PACT_DISABLE_SSL_VERIFICATION=false \
             -e PACT_BROKER_BASE_URL=$ctx.PACT_BROKER_URL -e PACT_BROKER_USERNAME=$ctx.pactUsername \
-            -e PACT_BROKER_PASSWORD=\$pbpassword pactfoundation/pact-cli:latest \
+            -e PACT_BROKER_PASSWORD=$ctx.pactPassword pactfoundation/pact-cli:latest \
             broker publish --consumer-app-version $version+$commitSha $pact --tag main
             """
             ctx.gitStatusWrapper(credentialsId: 'github-token', sha: Utils.getCommitSha(ctx), repo: Utils.getRepoName(ctx), gitHubContext: GitHubStatus.PactBrokerPublish.Context, description: GitHubStatus.PactBrokerPublish.Description) {
             def output = ctx.sh(returnStatus: true, script: script)
             ctx.echo "output from command: $output"
-            // output = 2 when successful
-            if (output == 1) {
-              ctx.error("Error occurred during publishing of pacts, pleaes check the log for further details.")
-            } else {
+
+            if (output == 2) {
               ctx.echo("Pacts published successfully.")
+            } else {
+              ctx.error("Error occurred during publishing of pacts, pleaes check the log for further details.")
             }
           }
         }
