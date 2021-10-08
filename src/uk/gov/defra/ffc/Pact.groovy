3b47ea362e20b2fb014fbb30316ff44b721a6e92
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-10-08 11:11:24 +0100

    test: trying some more stuff

--- src/uk/gov/defra/ffc/Pact.groovy
+++ src/uk/gov/defra/ffc/Pact.groovy
@@ -20,7 +20,7 @@ class Pact implements Serializable {
             set +x
             docker run --rm -w \$(pwd) -v \$(pwd):\$(pwd) -e PACT_DISABLE_SSL_VERIFICATION=false \
             -e PACT_BROKER_BASE_URL=$ctx.PACT_BROKER_URL -e PACT_BROKER_USERNAME=$ctx.pactUsername \
-            -e PACT_BROKER_PASSWORD=$ctx.pactPassword pactfoundation/pact-cli:latest \
+            -e PACT_BROKER_PASSWORD=$ctx.PACT_BROKER_PASSWORD pactfoundation/pact-cli:latest \
             broker publish --consumer-app-version $version+$commitSha $pact --tag main
             """
             ctx.gitStatusWrapper(credentialsId: 'github-token', sha: Utils.getCommitSha(ctx), repo: Utils.getRepoName(ctx), gitHubContext: GitHubStatus.PactBrokerPublish.Context, description: GitHubStatus.PactBrokerPublish.Description) {
