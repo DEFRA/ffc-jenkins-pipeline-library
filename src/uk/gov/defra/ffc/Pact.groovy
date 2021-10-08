5a82f3cd28e237df34822492e7b180be99066e27
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-10-08 08:44:53 +0100

    test: trying some more stuff

--- src/uk/gov/defra/ffc/Pact.groovy
+++ src/uk/gov/defra/ffc/Pact.groovy
@@ -36,10 +36,11 @@ class Pact implements Serializable {
           ctx.echo "pactuser01"
           ctx.echo 'pactuser01'
 
-          def script = "docker run --rm -w \$(pwd) -v \$(pwd):\$(pwd) -e PACT_DISABLE_SSL_VERIFICATION=false -e PACT_BROKER_BASE_URL=$ctx.PACT_BROKER_URL -e PACT_BROKER_USERNAME=$ctx.PACT_BROKER_USERNAME -e PACT_BROKER_PASSWORD=$ctx.PACT_BROKER_PASSWORD pactfoundation/pact-cli:latest broker publish --consumer-app-version $version+$commitSha $pact --tag main"
+          def script = "docker run --rm -w \$(pwd) -v \$(pwd):\$(pwd) -e PACT_DISABLE_SSL_VERIFICATION=false -e PACT_BROKER_BASE_URL=$ctx.PACT_BROKER_URL -e PACT_BROKER_USERNAME=$ctx.PACT_BROKER_USERNAME -e PACT_BROKER_PASSWORD=$ctx.pactPassword pactfoundation/pact-cli:latest broker publish --consumer-app-version $version+$commitSha $pact --tag main"
             ctx.gitStatusWrapper(credentialsId: 'github-token', sha: Utils.getCommitSha(ctx), repo: Utils.getRepoName(ctx), gitHubContext: GitHubStatus.PactBrokerPublish.Context, description: GitHubStatus.PactBrokerPublish.Description) {
             def output = ctx.sh(returnStatus: true, script: script)
             ctx.echo "output from command: $output"
+            // output = 2 when successful
           }
         }
       }
