9627d43e5b52e6be66c9cd67f10e5d85b764fd94
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-10-08 10:21:38 +0100

    test: trying some more stuff

--- src/uk/gov/defra/ffc/Pact.groovy
+++ src/uk/gov/defra/ffc/Pact.groovy
@@ -37,9 +37,9 @@ class Pact implements Serializable {
           ctx.echo 'pactuser01'
 
           def script = """
-            echo $ctx.pactPassword
-            pbpassword=\$(echo $ctx.pactPassword | base64 --decode)
-            echo $pbpassword
+            echo $ctx.pactPassword \
+            pbpassword=\$(echo $ctx.pactPassword | base64 --decode) \
+            echo $pbpassword \
             docker run --rm -w \$(pwd) -v \$(pwd):\$(pwd) -e PACT_DISABLE_SSL_VERIFICATION=false \
             -e PACT_BROKER_BASE_URL=$PACT_BROKER_URL -e PACT_BROKER_USERNAME=$ctx.pactUsername \
             -e PACT_BROKER_PASSWORD=$pbpassword pactfoundation/pact-cli:latest \
