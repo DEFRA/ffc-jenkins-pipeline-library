301b3aff6a7352c2fd9a68993b639702068990c1
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-10-08 11:50:38 +0100

    test: trying some more stuff

--- src/uk/gov/defra/ffc/Pact.groovy
+++ src/uk/gov/defra/ffc/Pact.groovy
@@ -19,18 +19,19 @@ class Pact implements Serializable {
           def script = '''
             docker run --rm -w \$(pwd) -v \$(pwd):\$(pwd) -e PACT_DISABLE_SSL_VERIFICATION=false \
             -e PACT_BROKER_BASE_URL=\$PACT_BROKER_URL -e PACT_BROKER_USERNAME=$pactUsername \
-            -e PACT_BROKER_PASSWORD="\$PACT_BROKER_PASSWORD" pactfoundation/pact-cli:latest \
+            -e PACT_BROKER_PASSWORD="$pactPassword" pactfoundation/pact-cli:latest \
             broker publish --consumer-app-version \
             ''' + version + '''+''' + commitSha + ''' ''' + pact + ''' --tag main
             '''
+
             ctx.gitStatusWrapper(credentialsId: 'github-token', sha: Utils.getCommitSha(ctx), repo: Utils.getRepoName(ctx), gitHubContext: GitHubStatus.PactBrokerPublish.Context, description: GitHubStatus.PactBrokerPublish.Description) {
             def output = ctx.sh(returnStatus: true, script: script)
             ctx.echo "output from command: $output"
 
-            if (output == 2) {
-              ctx.echo("Pacts published successfully.")
-            } else {
+            if (output == 1) {
               ctx.error("Error occurred during publishing of pacts, pleaes check the log for further details.")
+            } else {
+              ctx.echo("Pacts published successfully.")
             }
           }
         }
