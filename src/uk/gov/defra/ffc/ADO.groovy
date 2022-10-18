ac0c31c19542e958a2791685b76ed5ed98c23f58
John Watson <johnwatson484@hotmail.com>
2022-10-18 13:45:36 +0100

    Add database pipeline

--- src/uk/gov/defra/ffc/ADO.groovy
+++ src/uk/gov/defra/ffc/ADO.groovy
@@ -3,14 +3,25 @@ package uk.gov.defra.ffc
 class ADO implements Serializable {
 
   static void triggerPipeline(def ctx, String namespace, String chartName, String chartVersion) {
+    triggerDatabasePipeline(ctx, chartName, chartVersion)
     triggerHelmPipeline(ctx, namespace, chartName, chartVersion)
+  }
+
+  static void triggerDatabasePipeline(def ctx, String database, String version) {
+    ctx.withCredentials([
+      ctx.usernamePassword(credentialsId: 'ado-credentials', usernameVariable: 'username', passwordVariable: 'password')
+    ]) {
+      ctx.echo "Triggering ADO Database pipeline for ${database} ${version}"
+      String pipelineId = ctx.ADO_DATABASE_PIPELINE_ID
+      ctx.sh("curl -v -u $ctx.username:$ctx.password https://dev.azure.com/defragovuk/DEFRA-FFC/_apis/pipelines/${pipelineId}/runs?api-version=6.0-preview.1 -H 'Content-Type: application/json' -d '{\"templateParameters\": {\"database\":\"$database\",\"tagValue\":\"$version\"}}'")
+    }
   }
 
   static void triggerHelmPipeline(def ctx, String namespace, String chartName, String chartVersion) {
     ctx.withCredentials([
       ctx.usernamePassword(credentialsId: 'ado-credentials', usernameVariable: 'username', passwordVariable: 'password')
     ]) {
-      ctx.echo "Triggering ADO pipeline for ${chartName} ${chartVersion} in ${namespace}"
+      ctx.echo "Triggering ADO Helm pipeline for ${chartName} ${chartVersion} in ${namespace}"
       String pipelineId = ctx.ADO_HELM_PIPELINE_ID
       ctx.sh("curl -v -u $ctx.username:$ctx.password https://dev.azure.com/defragovuk/DEFRA-FFC/_apis/pipelines/${pipelineId}/runs?api-version=6.0-preview.1 -H 'Content-Type: application/json' -d '{\"templateParameters\": {\"namespace\": \"$namespace\",\"chart\":\"$chartName\",\"chartVersion\":\"$chartVersion\"}}'")
     }
