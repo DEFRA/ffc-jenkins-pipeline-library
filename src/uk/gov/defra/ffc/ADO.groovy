bd0011a90ee75dc7e239c4f30f35179ae1e6db58
John Watson <johnwatson484@hotmail.com>
2022-10-18 13:40:09 +0100

    Restructure

--- src/uk/gov/defra/ffc/ADO.groovy
+++ src/uk/gov/defra/ffc/ADO.groovy
@@ -3,12 +3,15 @@ package uk.gov.defra.ffc
 class ADO implements Serializable {
 
   static void triggerPipeline(def ctx, String namespace, String chartName, String chartVersion) {
+    triggerHelmPipeline(ctx, namespace, chartName, chartVersion)
+  }
+
+  static void triggerHelmPipeline(def ctx, String namespace, String chartName, String chartVersion) {
     ctx.withCredentials([
       ctx.usernamePassword(credentialsId: 'ado-credentials', usernameVariable: 'username', passwordVariable: 'password')
     ]) {
       ctx.echo "Triggering ADO pipeline for ${chartName} ${chartVersion} in ${namespace}"
       String pipelineId = ctx.ADO_HELM_PIPELINE_ID
-      ctx.echo "Pipeline ID: ${pipelineId}"
       ctx.sh("curl -v -u $ctx.username:$ctx.password https://dev.azure.com/defragovuk/DEFRA-FFC/_apis/pipelines/${pipelineId}/runs?api-version=6.0-preview.1 -H 'Content-Type: application/json' -d '{\"templateParameters\": {\"namespace\": \"$namespace\",\"chart\":\"$chartName\",\"chartVersion\":\"$chartVersion\"}}'")
     }
   }
