c3d07426c2683db4491fe191c4a42ca9b6cc39fe
John Watson <johnwatson484@hotmail.com>
2022-10-18 13:31:04 +0100

    Add logging

--- src/uk/gov/defra/ffc/ADO.groovy
+++ src/uk/gov/defra/ffc/ADO.groovy
@@ -6,8 +6,10 @@ class ADO implements Serializable {
     ctx.withCredentials([
       ctx.usernamePassword(credentialsId: 'ado-credentials', usernameVariable: 'username', passwordVariable: 'password')
     ]) {
+      ctx.echo "Triggering ADO pipeline for ${chartName} ${chartVersion} in ${namespace}"
       String pipelineId = ctx.ADO_HELM_PIPELINE_ID
-      ctx.sh("curl -u $ctx.username:$ctx.password https://dev.azure.com/defragovuk/DEFRA-FFC/_apis/pipelines/${pipelineId}/runs?api-version=6.0-preview.1 -H 'Content-Type: application/json' -d '{\"templateParameters\": {\"namespace\": \"${namespace}\",\"chart\":\"${chartName}\",\"chartVersion\":\"${chartVersion}\"}}'")      
+      ctx.echo "Pipeline ID: ${pipelineId}"
+      ctx.sh("curl -v -u $ctx.username:$ctx.password https://dev.azure.com/defragovuk/DEFRA-FFC/_apis/pipelines/${pipelineId}/runs?api-version=6.0-preview.1 -H 'Content-Type: application/json' -d '{\"templateParameters\": {\"namespace\": \"$namespace\",\"chart\":\"$chartName\",\"chartVersion\":\"$chartVersion\"}}'")
     }
   }
 }
