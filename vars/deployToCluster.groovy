2a7aa282a69cf57762c641c442f06ddc89ea2be6
John Watson <johnwatson484@hotmail.com>
2022-10-18 14:37:16 +0100

    Temp set db true

--- vars/deployToCluster.groovy
+++ vars/deployToCluster.groovy
@@ -1,14 +1,14 @@
 void call(Map config=[:], Closure body={}) {
-  Boolean hasDatabase = false
+  Boolean hasDatabase = true
 
   node {
     try {
-      stage('Deploy Database') {
-        hasDatabase = database.runRemoteMigrations(config.environment, config.chartName, config.chartVersion)
-      }
-      stage('Deploy Helm chart') {
-        helm.deployRemoteChart(config.environment, config.namespace, config.chartName, config.chartVersion, config.helmChartRepoType)
-      }
+      // stage('Deploy Database') {
+      //   hasDatabase = database.runRemoteMigrations(config.environment, config.chartName, config.chartVersion)
+      // }
+      // stage('Deploy Helm chart') {
+      //   helm.deployRemoteChart(config.environment, config.namespace, config.chartName, config.chartVersion, config.helmChartRepoType)
+      // }
       stage('Trigger ADO pipelines') {
         ado.triggerPipeline(config.namespace, config.chartName, config.chartVersion, hasDatabase)
       }
