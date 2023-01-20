6d50da6b37bce2ff980456faa871d2c3dc2e162c
John Watson <johnwatson484@hotmail.com>
2023-01-20 10:52:22 +0000

    Remove trigger ADO pipelines

--- vars/deployToCluster.groovy
+++ vars/deployToCluster.groovy
@@ -9,9 +9,6 @@ void call(Map config=[:], Closure body={}) {
       stage('Deploy Helm chart') {
         helm.deployRemoteChart(config.environment, config.namespace, config.chartName, config.chartVersion, config.helmChartRepoType)
       }
-      stage('Trigger ADO pipelines') {
-        ado.triggerPipeline(config.namespace, config.chartName, config.chartVersion, hasDatabase)
-      }
       body()
     } catch(e) {
       notifySlack.deploymentFailure()
