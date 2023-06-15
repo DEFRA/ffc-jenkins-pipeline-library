ab7a4ad80b9bdc9f124eac95400e70e1dc475d03
steve-dickinson <steve.dickinson@defra.gov.uk>
2023-06-15 12:56:57 +0100

    Add runtime value for creation of function

--- vars/function.groovy
+++ vars/function.groovy
@@ -1,5 +1,5 @@
 import uk.gov.defra.ffc.Function
 
-void createFunctionResources(String repoName, String pr, String gitToken, String defaultBranchstring, String runtime) {
+void createFunctionResources(String repoName, String pr, String gitToken, String defaultBranch, String runtime) {
   Function.createFunctionResources(this, repoName, pr, gitToken, defaultBranch, runtime)
 }
