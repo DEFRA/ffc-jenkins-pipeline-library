6b290a71ca6656175895c5d267762a0374fb178c
Farsid.Mirza <farsid.mirza@gmail.com>
2024-10-12 13:38:02 +0100

    test the jenkis pipline to create wid

--- vars/provision.groovy
+++ vars/provision.groovy
@@ -1,7 +1,7 @@
 import uk.gov.defra.ffc.Provision
 
 void createResources(String environment, String repoName, String tag, String pr) {
-  Provision.createResources(this, environment, repoName, pr)
+  Provision.createResources(this, environment, repoName, tag, pr)
 }
 
 void deleteBuildResources(String repoName, String pr) {
