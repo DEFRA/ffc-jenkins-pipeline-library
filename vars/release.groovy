ef7c84486b3771dbd166669f1f0b39e6692fd2aa
Farsid.Mirza <farsid.mirza@gmail.com>
2024-10-18 14:10:22 +0100

    create pre release

--- vars/release.groovy
+++ vars/release.groovy
@@ -1,7 +1,7 @@
 import uk.gov.defra.ffc.Release
 
-Boolean trigger(String versionTag, String repoName, String releaseDescription, String token) {
-  return Release.trigger(this, versionTag, repoName, releaseDescription, token)
+Boolean trigger(String versionTag, String repoName, String releaseDescription, String token, prerelease=false) {
+  return Release.trigger(this, versionTag, repoName, releaseDescription, token, prerelease)
 }
 
 void addSemverTags(String version, String repoName) {
